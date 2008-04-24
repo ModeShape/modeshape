99a23e8b75e171aabf228fe28d88788961fa01bf
Randall Hauch <rhauch@redhat.com>
2008-04-24 13:48:43 +0000

    Fixed a bad test case.
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@107 76366958-4244-0410-ad5e-bbfabb93f86b

--- dna-common/src/test/java/org/jboss/dna/common/AbstractI18nTest.java
+++ dna-common/src/test/java/org/jboss/dna/common/AbstractI18nTest.java
@@ -17,36 +17,35 @@ import org.junit.Test;
 /**
  * @author John Verhaeg
  */
-public class AbstractI18nTest {
+public abstract class AbstractI18nTest {
 
-	private Class i18nClass;
+    private Class i18nClass;
 
-	protected AbstractI18nTest( Class i18nClass ) {
-		this.i18nClass = i18nClass;
-	}
+    protected AbstractI18nTest( Class i18nClass ) {
+        this.i18nClass = i18nClass;
+    }
 
-	@Test
-	@SuppressWarnings( "unchecked" )
-	public void shouldNotHaveLocalizationProblems() throws Exception {
-		Method method = i18nClass.getDeclaredMethod("getLocalizationProblemLocales", (Class[])null);
-		Set<Locale> locales = (Set<Locale>)method.invoke(null, (Object[])null);
-		method = i18nClass.getDeclaredMethod("getLocalizationProblems", Locale.class);
-		for (Locale locale : locales) {
-			assertThat(((Set<String>)method.invoke(null, locale)).isEmpty(), is(true));
-		}
-	}
+    @Test
+    @SuppressWarnings( "unchecked" )
+    public void shouldNotHaveLocalizationProblems() throws Exception {
+        Method method = i18nClass.getDeclaredMethod("getLocalizationProblemLocales", (Class[])null);
+        Set<Locale> locales = (Set<Locale>)method.invoke(null, (Object[])null);
+        method = i18nClass.getDeclaredMethod("getLocalizationProblems", Locale.class);
+        for (Locale locale : locales) {
+            assertThat(((Set<String>)method.invoke(null, locale)).isEmpty(), is(true));
+        }
+    }
 
-	@Test
-	public void shouldNotHaveProblems() throws IllegalAccessException {
-		for (Field fld : i18nClass.getDeclaredFields()) {
-			if (fld.getType() == I18n.class && (fld.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC
-			    && (fld.getModifiers() & Modifier.STATIC) == Modifier.STATIC
-			    && (fld.getModifiers() & Modifier.FINAL) != Modifier.FINAL) {
-				I18n i18n = (I18n)fld.get(null);
-				if (i18n.hasProblem()) {
-					fail();
-				}
-			}
-		}
-	}
+    @Test
+    public void shouldNotHaveProblems() throws IllegalAccessException {
+        for (Field fld : i18nClass.getDeclaredFields()) {
+            if (fld.getType() == I18n.class && (fld.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC && (fld.getModifiers() & Modifier.STATIC) == Modifier.STATIC
+                && (fld.getModifiers() & Modifier.FINAL) != Modifier.FINAL) {
+                I18n i18n = (I18n)fld.get(null);
+                if (i18n.hasProblem()) {
+                    fail();
+                }
+            }
+        }
+    }
 }
