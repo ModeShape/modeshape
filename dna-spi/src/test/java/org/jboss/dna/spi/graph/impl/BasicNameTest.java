3157b572e389703fa6db4a4d2a6020122405b6b1
JPAV <jverhaeg@redhat.com>
2008-06-02 20:11:36 +0000

    DNA-94: Changed to throw IAE if name is null or empty
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@232 76366958-4244-0410-ad5e-bbfabb93f86b

--- dna-spi/src/test/java/org/jboss/dna/spi/graph/impl/BasicNameTest.java
+++ dna-spi/src/test/java/org/jboss/dna/spi/graph/impl/BasicNameTest.java
@@ -2,7 +2,7 @@
  * JBoss, Home of Professional Open Source.
  * Copyright 2008, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors. 
+ * distribution for a full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -32,6 +32,7 @@ import org.junit.Test;
 
 /**
  * @author Randall Hauch
+ * @author John Verhaeg
  */
 public class BasicNameTest {
 
@@ -74,16 +75,14 @@ public class BasicNameTest {
         assertThat(name.getNamespaceUri(), is(""));
     }
 
-    @Test
-    public void shouldAllowNullLocalNameInConstructorAndConvertToEmptyString() {
-        name = new BasicName(validNamespaceUri, null);
-        assertThat(name.getLocalName(), is(""));
+    @Test( expected = IllegalArgumentException.class )
+	public void shouldNotAllowNullLocalNameInConstructor() {
+		new BasicName(validNamespaceUri, null);
     }
 
-    @Test
-    public void shouldAllowEmptyLocalNameInConstructorAndConvertToEmptyString() {
-        name = new BasicName(validNamespaceUri, "");
-        assertThat(name.getLocalName(), is(""));
+    @Test( expected = IllegalArgumentException.class )
+	public void shouldAllowEmptyLocalNameInConstructor() {
+		new BasicName(validNamespaceUri, "");
     }
 
     @Test
