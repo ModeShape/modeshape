79e0532f72e3cfd93ce7f2f9eb41d4c06846a573
Randall Hauch <rhauch@redhat.com>
2008-06-10 20:18:05 +0000

    Added StringMatcher utility for use with JUnit tests.  The old matcher came with JMock, which we're trying to replace with Mockito, which doesn't have a "startsWith" matcher.
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@252 76366958-4244-0410-ad5e-bbfabb93f86b

---
+++ dna-common/src/test/java/org/jboss/dna/common/text/StringMatcher.java
@@ -0,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.dna.common.text;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.matchers.TypeSafeMatcher;
+
+/**
+ * @author Randall Hauch
+ */
+public class StringMatcher extends TypeSafeMatcher<String> {
+
+    protected final String substring;
+
+    protected StringMatcher( final String substring ) {
+        this.substring = substring;
+    }
+
+    @Override
+    public boolean matchesSafely( String item ) {
+        return item.startsWith(item);
+    }
+
+    public void describeTo( Description description ) {
+        description.appendText("a string starts with ").appendValue(substring);
+    }
+
+    public static Matcher<String> startsWith( String prefix ) {
+        return new StringMatcher(prefix);
+    }
+
+}
