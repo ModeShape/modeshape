98a1c9fc406d5b8598880ff03be91fdb5b0a1604
Randall Hauch <rhauch@redhat.com>
2008-06-10 22:01:20 +0000

    Added the required JBoss and copyright header.
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@258 76366958-4244-0410-ad5e-bbfabb93f86b

--- dna-common/src/test/java/org/jboss/dna/common/AbstractI18nTest.java
+++ dna-common/src/test/java/org/jboss/dna/common/AbstractI18nTest.java
@@ -1,5 +1,23 @@
 /*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
  *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.jboss.dna.common;
 
@@ -39,7 +57,8 @@ public abstract class AbstractI18nTest {
     @Test
     public void shouldNotHaveProblems() throws IllegalAccessException {
         for (Field fld : i18nClass.getDeclaredFields()) {
-            if (fld.getType() == I18n.class && (fld.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC && (fld.getModifiers() & Modifier.STATIC) == Modifier.STATIC
+            if (fld.getType() == I18n.class && (fld.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC
+                && (fld.getModifiers() & Modifier.STATIC) == Modifier.STATIC
                 && (fld.getModifiers() & Modifier.FINAL) != Modifier.FINAL) {
                 I18n i18n = (I18n)fld.get(null);
                 if (i18n.hasProblem()) {
