5d01f793578a8a28c3c1f30e14d82b2ec6c9ef8a
Brian Carothers <brian.carothers@amentra.com>
2009-06-15 14:54:13 +0000

    Added missing file to fix breaks in nightly build
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@1051 76366958-4244-0410-ad5e-bbfabb93f86b

---
+++ dna-graph/src/main/java/org/jboss/dna/graph/request/UuidConflictBehavior.java
@@ -0,0 +1,15 @@
+package org.jboss.dna.graph.request;
+
+/**
+ * An enumeration used by {@link CopyBranchRequest} for the choice of handling duplicate UUIDs, such as when a node is to be
+ * copied to another location where a node already exists.
+ * 
+ * @author Randall Hauch
+ */
+public enum UuidConflictBehavior {
+
+    ALWAYS_CREATE_NEW_UUID,
+    DO_NOT_COPY,
+    REPLACE_EXISTING_NODE,
+    THROW_EXCEPTION;
+}
