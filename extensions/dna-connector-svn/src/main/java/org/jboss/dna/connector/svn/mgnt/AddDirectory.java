c4dfd808a84f1d0522ee0f5cc588a5895587320c
Serge Pagop <serge.pagop@googlemail.com>
2009-12-29 22:46:08 +0000

    some write svn access (add file/dir, update file)
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@1489 76366958-4244-0410-ad5e-bbfabb93f86b

---
+++ extensions/dna-connector-svn/src/main/java/org/jboss/dna/connector/svn/mgnt/AddDirectory.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss DNA (http://www.jboss.org/dna)
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * See the AUTHORS.txt file in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * JBoss DNA is free software. Unless otherwise indicated, all code in JBoss DNA
+ * is licensed to you under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * JBoss DNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.dna.connector.svn.mgnt;
+
+import org.jboss.dna.connector.scm.ScmAction;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.io.ISVNEditor;
+
+/**
+ * root should be the last, previously created, parent folder. Each directory in the path will be created.
+ */
+public class AddDirectory implements ScmAction {
+    private String rootDirPath;
+    private String childDirPath;
+
+    public AddDirectory( String rootPath,
+                         String childDirPath ) {
+        this.rootDirPath = rootPath;
+        this.childDirPath = childDirPath;
+    }
+
+    public void applyAction( Object context ) throws SVNException {
+
+        ISVNEditor editor = (ISVNEditor)context;
+
+        ISVNEditorUtil.openDirectories(editor, this.rootDirPath);
+        String[] paths = this.childDirPath.split("/");
+        String newPath = this.rootDirPath;
+        for (int i = 0, length = paths.length; i < length; i++) {
+            newPath = (newPath.length() != 0) ? newPath + "/" + paths[i] : paths[i];
+
+            editor.addDir(newPath, null, -1);
+        }
+
+        ISVNEditorUtil.closeDirectories(editor, childDirPath);
+        ISVNEditorUtil.closeDirectories(editor, this.rootDirPath);
+    }
+}
