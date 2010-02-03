ffffa21d8e18598e8527225ba214b672e26a9765
Randall Hauch <rhauch@redhat.com>
2010-02-03 15:48:30 +0000

    MODE-656 Fixed the Duration test case so that it works in Locales that use something other than '.' for the decimal point delimiter (e.g., Germany).
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@1632 76366958-4244-0410-ad5e-bbfabb93f86b

--- modeshape-common/src/test/java/org/modeshape/common/math/DurationTest.java
+++ modeshape-common/src/test/java/org/modeshape/common/math/DurationTest.java
@@ -26,6 +26,7 @@ package org.modeshape.common.math;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
@@ -59,20 +60,34 @@ public class DurationTest {
         assertEquals(1.0d, this.duration.getComponents().getSeconds(), 0.00001d);
     }
 
+    /**
+     * Need to be careful about comparing strings, because {@link DecimalFormat} is used and relies upon the default locale. What
+     * we do know is:
+     * <ul>
+     * <li>the hours will be at least 2 digits (zero-padded at beginning);</li>
+     * <li>the minutes will be 2 digits (zero-padded at beginning);</li>
+     * <li>the seconds will be formatted according to "00.000,###" using DecimalFormat and the default locale (which may use a
+     * different decimal point delimiter than '.') and where the ',' is only used if there are more than 3 digits used in the
+     * fractional part;</li>
+     * <li>the hours, minutes and seconds will be delimited with a ':';</li>
+     */
     @Test
     public void shouldRepresentTimeInProperFormat() {
         this.duration = this.duration.add(2, TimeUnit.SECONDS);
-        assertEquals("00:00:02.000", this.duration.toString());
+        assertEquals(this.duration.toString().startsWith("00:00:02"), true);
+        assertEquals(this.duration.toString().endsWith("000"), true);
 
         this.duration = new Duration(1100, TimeUnit.MILLISECONDS);
         this.duration = this.duration.add(1 * 60, TimeUnit.SECONDS);
         this.duration = this.duration.add(1 * 60 * 60, TimeUnit.SECONDS);
-        assertEquals("01:01:01.100", this.duration.toString());
+        assertEquals(this.duration.toString().startsWith("01:01:01"), true);
+        assertEquals(this.duration.toString().endsWith("100"), true);
 
         this.duration = new Duration(30100123, TimeUnit.MICROSECONDS);
         this.duration = this.duration.add(20 * 60, TimeUnit.SECONDS);
         this.duration = this.duration.add(10 * 60 * 60, TimeUnit.SECONDS);
-        assertEquals("10:20:30.100,123", this.duration.toString());
+        assertEquals(this.duration.toString().startsWith("10:20:30"), true);
+        assertEquals(this.duration.toString().endsWith("100,123"), true);
     }
 
 }
