2f698b4c21bb7a5ddfcb582031395ab0e688277a
Van Halbert <vhalbert@redhat.com>
2010-07-29 19:44:59 +0000

    MODE-813 -  kit rearrangement.   The new kit is called modeshape-jbossas-dist.zip.     The new context are:   modeshape-rest and modeshape-webdav.
    git-svn-id: https://svn.jboss.org/repos/modeshape/trunk@2060 76366958-4244-0410-ad5e-bbfabb93f86b

--- deploy/jbossas/modeshape-jbossas-web-rest-war/src/test/java/org/modeshape/web/jcr/rest/JcrResourcesTest.java
+++ deploy/jbossas/modeshape-jbossas-web-rest-war/src/test/java/org/modeshape/web/jcr/rest/JcrResourcesTest.java
@@ -50,7 +50,7 @@ import org.modeshape.common.util.Base64;
  */
 public class JcrResourcesTest {
 
-    private static final String SERVER_CONTEXT = "/resources";
+    private static final String SERVER_CONTEXT = "/modeshape";
     private static final String SERVER_URL = "http://localhost:8090" + SERVER_CONTEXT;
 
     @Before
@@ -144,7 +144,7 @@ public class JcrResourcesTest {
 
         JSONObject objFromResponse = new JSONObject(body);
         JSONObject expected = new JSONObject(
-                                             "{\"mode%3arepository\":{\"repository\":{\"name\":\"mode%3arepository\",\"resources\":{\"workspaces\":\"/resources/mode%3arepository\"}}}}");
+                                             "{\"mode%3arepository\":{\"repository\":{\"name\":\"mode%3arepository\",\"resources\":{\"workspaces\":\"/mode:resources/mode%3arepository\"}}}}");
 
         assertThat(connection.getResponseCode(), is(HttpURLConnection.HTTP_OK));
         assertThat(objFromResponse.toString(), is(expected.toString()));
@@ -163,7 +163,7 @@ public class JcrResourcesTest {
 
         JSONObject objFromResponse = new JSONObject(body);
         JSONObject expected = new JSONObject(
-                                             "{\"default\":{\"workspace\":{\"name\":\"default\",\"resources\":{\"items\":\"/resources/mode%3arepository/default/items\"}}}}");
+                                             "{\"default\":{\"workspace\":{\"name\":\"default\",\"resources\":{\"items\":\"/mode:resources/mode%3arepository/default/items\"}}}}");
 
         assertThat(connection.getResponseCode(), is(HttpURLConnection.HTTP_OK));
         assertThat(objFromResponse.toString(), is(expected.toString()));
