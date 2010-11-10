b11672866e7c9908958c8f77d066d1b3a4c911b7
Randall Hauch <rhauch@redhat.com>
2010-11-10 20:53:28 +0000

    MODE-998 Add support to bind the JCR Repository to a JNDI location.
    git-svn-id: https://svn.jboss.org/repos/modeshape/branches/2.2.x@2522 76366958-4244-0410-ad5e-bbfabb93f86b

--- modeshape-jcr/src/main/java/org/modeshape/jcr/JcrI18n.java
+++ modeshape-jcr/src/main/java/org/modeshape/jcr/JcrI18n.java
@@ -119,6 +119,7 @@ public final class JcrI18n {
     public static I18n constraintViolatedOnReference;
     public static I18n invalidOptionProvided;
     public static I18n noOptionValueProvided;
+    public static I18n unableToBindToJndi;
 
     public static I18n cannotRemoveRootNode;
     public static I18n cannotRemoveParentNodeOfTarget;
