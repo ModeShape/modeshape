7c67709d4e865509d80cfd411fb36edd20986337
Randall Hauch <rhauch@gmail.com>
2011-11-11 13:57:57 -0600

    MODE-1298 Added more unit tests to help diagnose
    Added several unit tests, plus some 'toString', 'equals', and 'hashCode' methods to the ValueReferences
    class and a 'toString' method to the ValueReference class.
    
    This commit includes no changes that attempt to fix the problem.

--- modeshape-jcr-api/src/main/java/org/modeshape/jcr/api/JaasCredentials.java
+++ modeshape-jcr-api/src/main/java/org/modeshape/jcr/api/JaasCredentials.java
@@ -44,7 +44,7 @@ public final class JaasCredentials implements Credentials {
      * Create a credentials that uses the supplied JAAS LoginContext for authentication and authorization.
      * 
      * @param loginContext the JAAS login context
-     * @throws <code>IllegalArgumentException</code> in case the given argument is null.
+     * @throws IllegalArgumentException in case the given argument is null.
      */
     public JaasCredentials( LoginContext loginContext ) {
         if (loginContext == null) {
