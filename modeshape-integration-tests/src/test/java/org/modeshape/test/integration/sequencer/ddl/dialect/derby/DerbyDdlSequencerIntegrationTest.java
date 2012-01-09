acee4feaa035f928fcb7fb988aaf59e0af4cd0c4
Horia Chiorean <hchiorea@redhat.com>
2012-01-09 15:04:19 +0200

    MODE-1362 Cleaned-up ddl integration tests and added test method for validating content

--- modeshape-integration-tests/src/test/java/org/modeshape/test/integration/sequencer/ddl/dialect/derby/DerbyDdlSequencerIntegrationTest.java
+++ modeshape-integration-tests/src/test/java/org/modeshape/test/integration/sequencer/ddl/dialect/derby/DerbyDdlSequencerIntegrationTest.java
@@ -23,197 +23,136 @@
  */
 package org.modeshape.test.integration.sequencer.ddl.dialect.derby;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
-import javax.jcr.SimpleCredentials;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-import org.modeshape.graph.connector.inmemory.InMemoryRepositorySource;
-import org.modeshape.jcr.JaasTestUtil;
-import org.modeshape.jcr.JcrConfiguration;
-import org.modeshape.jcr.JcrTools;
-import org.modeshape.sequencer.ddl.StandardDdlLexicon;
 import org.modeshape.sequencer.ddl.dialect.derby.DerbyDdlLexicon;
-import org.modeshape.test.integration.sequencer.ddl.DdlIntegrationTestUtil;
+import org.modeshape.test.integration.sequencer.ddl.AbstractDdlIntegrationTest;
 
 /**
+ * Integration test for the {@link org.modeshape.sequencer.ddl.DdlSequencer} using a Derby dialect.
+ *
  * @author blafond
+ * @author Horia Chiorean
  */
-public class DerbyDdlSequencerIntegrationTest extends DdlIntegrationTestUtil {
-    private String resourceFolder = ddlTestResourceRootFolder + "/dialect/derby/";
+public class DerbyDdlSequencerIntegrationTest extends AbstractDdlIntegrationTest {
+    private static final String RESOURCE_FOLDER = DDL_TEST_RESOURCE_ROOT_FOLDER + "/dialect/derby/";
 
-    @BeforeClass
-    public static void beforeAll() {
-        // Initialize the JAAS configuration to allow for an admin login later
-        JaasTestUtil.initJaas("security/jaas.conf.xml");
-    }
-
-    @AfterClass
-    public static void afterAll() {
-        JaasTestUtil.releaseJaas();
-    }
-
-    @Before
-    public void beforeEach() throws Exception {
-        // Configure the ModeShape configuration. This could be done by loading a configuration from a file, or by
-        // using a (local or remote) configuration repository, or by setting up the configuration programmatically.
-        // This test uses the programmatic approach...
-
-        tools = new JcrTools();
-
-        String repositoryName = "ddlRepository";
-        String workspaceName = "default";
-        String repositorySource = "ddlRepositorySource";
-
-        JcrConfiguration config = new JcrConfiguration();
-        // Set up the in-memory source where we'll upload the content and where the sequenced output will be stored ...
-        config.repositorySource(repositorySource).usingClass(InMemoryRepositorySource.class).setDescription("The repository for our content").setProperty("defaultWorkspaceName",
-                                                                                                                                                          workspaceName);
-        // Set up the JCR repository to use the source ..protected.
-        config.repository(repositoryName).addNodeTypes(getUrl("org/modeshape/sequencer/ddl/StandardDdl.cnd")).addNodeTypes(getUrl(resourceFolder
-                                                                                                                                  + "DerbyDdl.cnd")).registerNamespace(StandardDdlLexicon.Namespace.PREFIX,
-                                                                                                                                                                       StandardDdlLexicon.Namespace.URI).registerNamespace(DerbyDdlLexicon.Namespace.PREFIX,
-                                                                                                                                                                                                                           DerbyDdlLexicon.Namespace.URI).setSource(repositorySource);
-        // Set up the DDL sequencer ...
-        config.sequencer("DDL Sequencer").usingClass("org.modeshape.sequencer.ddl.DdlSequencer").loadedFromClasspath().setDescription("Sequences DDL files to extract individual statements and accompanying statement properties and values").sequencingFrom("(//(*.(ddl)[*]))/jcr:content[@jcr:data]").andOutputtingTo("/ddls/$1");
-        config.save();
-        this.engine = config.build();
-        this.engine.start();
-
-        this.session = this.engine.getRepository(repositoryName).login(new SimpleCredentials("superuser",
-                                                                                             "superuser".toCharArray()),
-                                                                       workspaceName);
-
-    }
-
-    @After
-    public void afterEach() throws Exception {
-        if (this.session != null) {
-            this.session.logout();
-        }
-        if (this.engine != null) {
-            this.engine.shutdown();
-        }
+    @Override
+    protected void addCustomConfiguration() {
+        config.repository(DEFAULT_REPOSITORY_NAME)
+              .addNodeTypes(getUrl(RESOURCE_FOLDER + "DerbyDdl.cnd"))
+              .registerNamespace(DerbyDdlLexicon.Namespace.PREFIX, DerbyDdlLexicon.Namespace.URI);
     }
 
     @Test
     public void shouldSequenceDerbyDdlFile() throws Exception {
 
-        uploadFile(resourceFolder, "derby_test_statements.ddl", "shouldSequenceDerbyDdlFile");
+        uploadFile(RESOURCE_FOLDER, "derby_test_statements.ddl");
 
         waitUntilSequencedNodesIs(1);
 
         // Find the node ...
-        Node root = session.getRootNode();
+        Node ddlsNode = getStatementsContainer();
 
-        if (root.hasNode("ddls")) {
-            if (root.hasNode("ddls")) {
-                Node ddlsNode = root.getNode("ddls/a/b");
-                // System.out.println("   | NAME: " + ddlsNode.getName() + "  PATH: " + ddlsNode.getPath());
-                for (NodeIterator iter = ddlsNode.getNodes(); iter.hasNext();) {
-                    Node ddlNode = iter.nextNode();
+        for (NodeIterator iter = ddlsNode.getNodes(); iter.hasNext(); ) {
+            Node ddlNode = iter.nextNode();
 
-                    long numStatements = ddlNode.getNodes().nextNode().getNodes().getSize();
-                    assertEquals(numStatements, 64);
+            long numStatements = ddlNode.getNodes().nextNode().getNodes().getSize();
+            assertEquals(numStatements, 64);
 
-                    // printNodeProperties(ddlNode);
+            // printNodeProperties(ddlNode);
 
-                    verifyNode(ddlNode, "HOTELAVAILABILITY", "ddl:startLineNumber");
-                    verifyNode(ddlNode, "SAMP.DEPARTMENT", "ddl:expression");
-                    verifyNode(ddlNode, "HOTEL_ID", "ddl:datatypeName");
-                    verifyNode(ddlNode, "CITIES", "ddl:startLineNumber");
+            verifyNode(ddlNode, "HOTELAVAILABILITY", "ddl:startLineNumber");
+            verifyNode(ddlNode, "SAMP.DEPARTMENT", "ddl:expression");
+            verifyNode(ddlNode, "HOTEL_ID", "ddl:datatypeName");
+            verifyNode(ddlNode, "CITIES", "ddl:startLineNumber");
 
-                    // Create Function
-                    verifyNode(ddlNode, "PROPERTY_FILE_READER", "ddl:startLineNumber", 71);
-                    verifyNodeTypes(ddlNode,
-                                    "PROPERTY_FILE_READER",
-                                    "derbyddl:createFunctionStatement",
-                                    "ddl:creatable",
-                                    "derbyddl:functionOperand");
-                    verifyNode(ddlNode, "KEY_COL", "ddl:datatypeName", "VARCHAR");
+            // Create Function
+            verifyNode(ddlNode, "PROPERTY_FILE_READER", "ddl:startLineNumber", 71);
+            verifyNodeTypes(ddlNode,
+                            "PROPERTY_FILE_READER",
+                            "derbyddl:createFunctionStatement",
+                            "ddl:creatable",
+                            "derbyddl:functionOperand");
+            verifyNode(ddlNode, "KEY_COL", "ddl:datatypeName", "VARCHAR");
 
-                    Node functionNode = findNode(ddlNode, "TO_DEGREES");
-                    assertNotNull(functionNode);
-                    verifyChildNode(functionNode, "parameterStyle", "ddl:value", "PARAMETER STYLE JAVA");
+            Node functionNode = findNode(ddlNode, "TO_DEGREES");
+            assertNotNull(functionNode);
+            verifyChildNode(functionNode, "parameterStyle", "ddl:value", "PARAMETER STYLE JAVA");
 
-                    // Create Index
-                    // CREATE INDEX IXSALE ON SAMP.SALES (SALES);
-                    Node indexNode = findNode(ddlNode, "IXSALE", "derbyddl:createIndexStatement");
-                    assertNotNull(indexNode);
-                    verifySimpleStringProperty(indexNode, "derbyddl:tableName", "SAMP.SALES");
-                    Node colRefNode = findNode(indexNode, "SALES");
-                    assertNotNull(colRefNode);
-                    colRefNode = findNode(ddlNode, "SALES", "derbyddl:indexColumnReference");
-                    assertNotNull(colRefNode);
-                    verifyNodeTypes(colRefNode,
-                                    "SALES",
-                                    "derbyddl:indexColumnReference",
-                                    "ddl:columnReference",
-                                    "ddl:referenceOperand");
+            // Create Index
+            // CREATE INDEX IXSALE ON SAMP.SALES (SALES);
+            Node indexNode = findNode(ddlNode, "IXSALE", "derbyddl:createIndexStatement");
+            assertNotNull(indexNode);
+            verifySimpleStringProperty(indexNode, "derbyddl:tableName", "SAMP.SALES");
+            Node colRefNode = findNode(indexNode, "SALES");
+            assertNotNull(colRefNode);
+            colRefNode = findNode(ddlNode, "SALES", "derbyddl:indexColumnReference");
+            assertNotNull(colRefNode);
+            verifyNodeTypes(colRefNode,
+                            "SALES",
+                            "derbyddl:indexColumnReference",
+                            "ddl:columnReference",
+                            "ddl:referenceOperand");
 
-                    // declare global temporary table SESSION.t1(c11 int) not logged;
-                    Node ttNode = findNode(ddlNode, "SESSION.t1", "derbyddl:declareGlobalTemporaryTableStatement");
-                    assertNotNull(ttNode);
-                    Node colNode = findNode(ttNode, "c11");
-                    assertNotNull(colNode);
-                    verifySimpleStringProperty(colNode, "ddl:datatypeName", "int");
+            // declare global temporary table SESSION.t1(c11 int) not logged;
+            Node ttNode = findNode(ddlNode, "SESSION.t1", "derbyddl:declareGlobalTemporaryTableStatement");
+            assertNotNull(ttNode);
+            Node colNode = findNode(ttNode, "c11");
+            assertNotNull(colNode);
+            verifySimpleStringProperty(colNode, "ddl:datatypeName", "int");
 
-                    // LOCK TABLE FlightAvailability IN EXCLUSIVE MODE;
-                    Node lockNode = findNode(ddlNode, "FlightAvailability", "derbyddl:lockTableStatement");
-                    assertNotNull(lockNode);
-                    Node optionNode = findNode(lockNode, "lockMode");
-                    assertNotNull(optionNode);
-                    verifySimpleStringProperty(optionNode, "ddl:value", "EXCLUSIVE");
+            // LOCK TABLE FlightAvailability IN EXCLUSIVE MODE;
+            Node lockNode = findNode(ddlNode, "FlightAvailability", "derbyddl:lockTableStatement");
+            assertNotNull(lockNode);
+            Node optionNode = findNode(lockNode, "lockMode");
+            assertNotNull(optionNode);
+            verifySimpleStringProperty(optionNode, "ddl:value", "EXCLUSIVE");
 
-                    // RENAME TABLE SAMP.EMP_ACT TO EMPLOYEE_ACT
-                    Node renameTableNode = findNode(ddlNode, "SAMP.EMP_ACT", "derbyddl:renameTableStatement");
-                    assertNotNull(renameTableNode);
-                    verifySimpleStringProperty(renameTableNode, "ddl:newName", "EMPLOYEE_ACT");
+            // RENAME TABLE SAMP.EMP_ACT TO EMPLOYEE_ACT
+            Node renameTableNode = findNode(ddlNode, "SAMP.EMP_ACT", "derbyddl:renameTableStatement");
+            assertNotNull(renameTableNode);
+            verifySimpleStringProperty(renameTableNode, "ddl:newName", "EMPLOYEE_ACT");
 
-                    // CREATE SYNONYM SAMP.T1 FOR SAMP.TABLEWITHLONGNAME;
-                    Node synonymNode = findNode(ddlNode, "SAMP.T1", "derbyddl:createSynonymStatement");
-                    assertNotNull(synonymNode);
-                    verifySimpleStringProperty(synonymNode, "derbyddl:tableName", "SAMP.TABLEWITHLONGNAME");
+            // CREATE SYNONYM SAMP.T1 FOR SAMP.TABLEWITHLONGNAME;
+            Node synonymNode = findNode(ddlNode, "SAMP.T1", "derbyddl:createSynonymStatement");
+            assertNotNull(synonymNode);
+            verifySimpleStringProperty(synonymNode, "derbyddl:tableName", "SAMP.TABLEWITHLONGNAME");
 
-                    // CREATE TRIGGER FLIGHTSDELETE3
-                    // AFTER DELETE ON FLIGHTS
-                    // REFERENCING OLD AS OLD
-                    // FOR EACH ROW
-                    // DELETE FROM FLIGHTAVAILABILITY WHERE FLIGHT_ID = OLD.FLIGHT_ID;
-                    Node triggerNode = findNode(ddlNode, "FLIGHTSDELETE3", "derbyddl:createTriggerStatement");
-                    assertNotNull(triggerNode);
-                    verifySimpleStringProperty(triggerNode, "derbyddl:tableName", "FLIGHTS");
+            // CREATE TRIGGER FLIGHTSDELETE3
+            // AFTER DELETE ON FLIGHTS
+            // REFERENCING OLD AS OLD
+            // FOR EACH ROW
+            // DELETE FROM FLIGHTAVAILABILITY WHERE FLIGHT_ID = OLD.FLIGHT_ID;
+            Node triggerNode = findNode(ddlNode, "FLIGHTSDELETE3", "derbyddl:createTriggerStatement");
+            assertNotNull(triggerNode);
+            verifySimpleStringProperty(triggerNode, "derbyddl:tableName", "FLIGHTS");
 
-                    // CREATE TRIGGER t1 NO CASCADE BEFORE UPDATE ON x
-                    // FOR EACH ROW MODE DB2SQL
-                    // values app.notifyEmail('Jerry', 'Table x is about to be updated');
-                    triggerNode = findNode(ddlNode, "t1", "derbyddl:createTriggerStatement");
-                    assertNotNull(triggerNode);
-                    verifySimpleStringProperty(triggerNode, "derbyddl:tableName", "x");
-                    optionNode = findNode(triggerNode, "forEach");
-                    assertNotNull(optionNode);
-                    verifySimpleStringProperty(optionNode, "ddl:value", "FOR EACH ROW");
-                    optionNode = findNode(triggerNode, "eventType");
-                    assertNotNull(optionNode);
-                    verifySimpleStringProperty(optionNode, "ddl:value", "UPDATE");
+            // CREATE TRIGGER t1 NO CASCADE BEFORE UPDATE ON x
+            // FOR EACH ROW MODE DB2SQL
+            // values app.notifyEmail('Jerry', 'Table x is about to be updated');
+            triggerNode = findNode(ddlNode, "t1", "derbyddl:createTriggerStatement");
+            assertNotNull(triggerNode);
+            verifySimpleStringProperty(triggerNode, "derbyddl:tableName", "x");
+            optionNode = findNode(triggerNode, "forEach");
+            assertNotNull(optionNode);
+            verifySimpleStringProperty(optionNode, "ddl:value", "FOR EACH ROW");
+            optionNode = findNode(triggerNode, "eventType");
+            assertNotNull(optionNode);
+            verifySimpleStringProperty(optionNode, "ddl:value", "UPDATE");
 
-                    // GRANT EXECUTE ON PROCEDURE p TO george;
-                    Node grantNode = findNode(ddlNode, "p", "derbyddl:grantOnProcedureStatement");
-                    assertNotNull(grantNode);
+            // GRANT EXECUTE ON PROCEDURE p TO george;
+            Node grantNode = findNode(ddlNode, "p", "derbyddl:grantOnProcedureStatement");
+            assertNotNull(grantNode);
 
-                    // GRANT purchases_reader_role TO george,maria;
-                    grantNode = findNode(ddlNode, "grantRoles", "derbyddl:grantRolesStatement");
-                    assertNotNull(grantNode);
-                    Node roleNode = findNode(grantNode, "george", "ddl:grantee");
-                    assertNotNull(roleNode);
-
-                }
-            }
+            // GRANT purchases_reader_role TO george,maria;
+            grantNode = findNode(ddlNode, "grantRoles", "derbyddl:grantRolesStatement");
+            assertNotNull(grantNode);
+            Node roleNode = findNode(grantNode, "george", "ddl:grantee");
+            assertNotNull(roleNode);
         }
     }
 }
--- modeshape-integration-tests/src/test/java/org/modeshape/test/integration/sequencer/ddl/dialect/postgres/PostgresDdlSequencerIntegrationTest.java
+++ modeshape-integration-tests/src/test/java/org/modeshape/test/integration/sequencer/ddl/dialect/postgres/PostgresDdlSequencerIntegrationTest.java
@@ -23,143 +23,75 @@
  */
 package org.modeshape.test.integration.sequencer.ddl.dialect.postgres;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
-import javax.jcr.SimpleCredentials;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-import org.modeshape.graph.connector.inmemory.InMemoryRepositorySource;
-import org.modeshape.jcr.JaasTestUtil;
-import org.modeshape.jcr.JcrConfiguration;
-import org.modeshape.jcr.JcrTools;
-import org.modeshape.sequencer.ddl.StandardDdlLexicon;
 import org.modeshape.sequencer.ddl.dialect.postgres.PostgresDdlLexicon;
-import org.modeshape.test.integration.sequencer.ddl.DdlIntegrationTestUtil;
+import org.modeshape.test.integration.sequencer.ddl.AbstractDdlIntegrationTest;
 
 /**
+ * Integration test for the {@link org.modeshape.sequencer.ddl.DdlSequencer} using a PostgreSQL dialect.
  *
+ * @author ?
+ * @author Horia Chiorean
  */
-public class PostgresDdlSequencerIntegrationTest extends DdlIntegrationTestUtil {
-    private String resourceFolder = ddlTestResourceRootFolder + "/dialect/postgres/";
+public class PostgresDdlSequencerIntegrationTest extends AbstractDdlIntegrationTest {
+    private static final String RESOURCE_FOLDER = DDL_TEST_RESOURCE_ROOT_FOLDER + "/dialect/postgres/";
 
-    @BeforeClass
-    public static void beforeAll() {
-        // Initialize the JAAS configuration to allow for an admin login later
-        JaasTestUtil.initJaas("security/jaas.conf.xml");
-    }
-
-    @AfterClass
-    public static void afterAll() {
-        JaasTestUtil.releaseJaas();
-    }
-
-    @Before
-    public void beforeEach() throws Exception {
-        // Configure the ModeShape configuration. This could be done by loading a configuration from a file, or by
-        // using a (local or remote) configuration repository, or by setting up the configuration programmatically.
-        // This test uses the programmatic approach...
-
-        tools = new JcrTools();
-
-        String repositoryName = "ddlRepository";
-        String workspaceName = "default";
-        String repositorySource = "ddlRepositorySource";
-
-        JcrConfiguration config = new JcrConfiguration();
-        // Set up the in-memory source where we'll upload the content and where the sequenced output will be stored ...
-        config.repositorySource(repositorySource)
-              .usingClass(InMemoryRepositorySource.class)
-              .setDescription("The repository for our content")
-              .setProperty("defaultWorkspaceName", workspaceName);
-        // Set up the JCR repository to use the source ...
-        config.repository(repositoryName)
-              .addNodeTypes(getUrl("org/modeshape/sequencer/ddl/StandardDdl.cnd"))
-              .addNodeTypes(getUrl(resourceFolder + "PostgresDdl.cnd"))
-              .registerNamespace(StandardDdlLexicon.Namespace.PREFIX, StandardDdlLexicon.Namespace.URI)
-              .registerNamespace(PostgresDdlLexicon.Namespace.PREFIX, PostgresDdlLexicon.Namespace.URI)
-              .setSource(repositorySource);
-        // Set up the DDL sequencer ...
-        config.sequencer("DDL Sequencer")
-              .usingClass("org.modeshape.sequencer.ddl.DdlSequencer")
-              .loadedFromClasspath()
-              .setDescription("Sequences DDL files to extract individual statements and accompanying statement properties and values")
-              .sequencingFrom("(//(*.(ddl)[*]))/jcr:content[@jcr:data]")
-              .andOutputtingTo("/ddls/$1");
-        config.save();
-        this.engine = config.build();
-        this.engine.start();
-
-        this.session = this.engine.getRepository(repositoryName).login(new SimpleCredentials("superuser",
-                                                                                             "superuser".toCharArray()),
-                                                                       workspaceName);
-
-    }
-
-    @After
-    public void afterEach() throws Exception {
-        if (this.session != null) {
-            this.session.logout();
-        }
-        if (this.engine != null) {
-            this.engine.shutdown();
-        }
+    @Override
+    protected void addCustomConfiguration() {
+        config.repository(DEFAULT_REPOSITORY_NAME)
+                .addNodeTypes(getUrl(RESOURCE_FOLDER + "PostgresDdl.cnd"))
+                .registerNamespace(PostgresDdlLexicon.Namespace.PREFIX, PostgresDdlLexicon.Namespace.URI);
     }
 
     @Test
     public void shouldSequencePostgresDdlFile() throws Exception {
 
-        uploadFile(resourceFolder, "postgres_test_statements.ddl", "shouldSequencePostgresDdlFile");
+        uploadFile(RESOURCE_FOLDER, "postgres_test_statements.ddl");
 
         waitUntilSequencedNodesIs(1);
 
         // Find the node ...
-        Node root = session.getRootNode();
+        Node ddlsNode = getStatementsContainer();
+        for (NodeIterator iter = ddlsNode.getNodes(); iter.hasNext(); ) {
+            Node ddlNode = iter.nextNode();
 
-        if (root.hasNode("ddls")) {
-            Node ddlsNode = root.getNode("ddls/a/b");
-            // System.out.println("   | NAME: " + ddlsNode.getName() + "  PATH: " + ddlsNode.getPath());
-            for (NodeIterator iter = ddlsNode.getNodes(); iter.hasNext();) {
-                Node ddlNode = iter.nextNode();
+            long numStatements = ddlNode.getNodes().nextNode().getNodes().getSize();
+            assertEquals(numStatements, 106);
 
-                long numStatements = ddlNode.getNodes().nextNode().getNodes().getSize();
-                assertEquals(numStatements, 106);
+            // printNodeProperties(ddlNode);
 
-                // printNodeProperties(ddlNode);
+            verifyNodeType(ddlNode, "increment", "postgresddl:createFunctionStatement");
+            verifyNode(ddlNode, "increment", "ddl:expression");
+            verifyNodeType(ddlNode, "increment", "ddl:creatable");
+            verifyNodeType(ddlNode, "increment", "postgresddl:functionOperand");
+            verifyNode(ddlNode, "increment", "ddl:startLineNumber", 214);
+            verifyNode(ddlNode, "increment", "ddl:startCharIndex", 7604);
 
-                verifyNodeType(ddlNode, "increment", "postgresddl:createFunctionStatement");
-                verifyNode(ddlNode, "increment", "ddl:expression");
-                verifyNodeType(ddlNode, "increment", "ddl:creatable");
-                verifyNodeType(ddlNode, "increment", "postgresddl:functionOperand");
-                verifyNode(ddlNode, "increment", "ddl:startLineNumber", 214);
-                verifyNode(ddlNode, "increment", "ddl:startCharIndex", 7604);
+            // COMMENT ON FUNCTION my_function (timestamp) IS ’Returns Roman Numeral’;
+            verifyNodeType(ddlNode, "my_function", "postgresddl:commentOnStatement");
+            verifyNode(ddlNode, "my_function", "ddl:expression");
+            verifyNodeType(ddlNode, "my_function", "postgresddl:commentOperand");
+            verifyNode(ddlNode, "my_function", "ddl:startLineNumber", 44);
+            verifyNode(ddlNode, "my_function", "ddl:startCharIndex", 1573);
+            verifyNode(ddlNode, "my_function", "postgresddl:comment", "'Returns Roman Numeral'");
 
-                // COMMENT ON FUNCTION my_function (timestamp) IS ’Returns Roman Numeral’;
-                verifyNodeType(ddlNode, "my_function", "postgresddl:commentOnStatement");
-                verifyNode(ddlNode, "my_function", "ddl:expression");
-                verifyNodeType(ddlNode, "my_function", "postgresddl:commentOperand");
-                verifyNode(ddlNode, "my_function", "ddl:startLineNumber", 44);
-                verifyNode(ddlNode, "my_function", "ddl:startCharIndex", 1573);
-                verifyNode(ddlNode, "my_function", "postgresddl:comment", "'Returns Roman Numeral'");
+            // ALTER TABLE foreign_companies RENAME COLUMN address TO city;
+            Node alterTableNode = findNode(ddlNode, "foreign_companies", "postgresddl:alterTableStatement");
+            assertNotNull(alterTableNode);
+            Node renameColNode = findNode(alterTableNode, "address", "postgresddl:renamedColumn");
+            assertNotNull(renameColNode);
+            verifySingleValueProperty(renameColNode, "ddl:newName", "city");
 
-                // ALTER TABLE foreign_companies RENAME COLUMN address TO city;
-                Node alterTableNode = findNode(ddlNode, "foreign_companies", "postgresddl:alterTableStatement");
-                assertNotNull(alterTableNode);
-                Node renameColNode = findNode(alterTableNode, "address", "postgresddl:renamedColumn");
-                assertNotNull(renameColNode);
-                verifySingleValueProperty(renameColNode, "ddl:newName", "city");
-
-                // GRANT EXECUTE ON FUNCTION divideByTwo(numerator int, IN demoninator int) TO george;
-                Node grantNode = findNode(ddlNode, "divideByTwo", "postgresddl:grantOnFunctionStatement");
-                assertNotNull(grantNode);
-                Node parameter_1 = findNode(grantNode, "numerator", "postgresddl:functionParameter");
-                assertNotNull(parameter_1);
-                verifySingleValueProperty(parameter_1, "ddl:datatypeName", "int");
-            }
+            // GRANT EXECUTE ON FUNCTION divideByTwo(numerator int, IN demoninator int) TO george;
+            Node grantNode = findNode(ddlNode, "divideByTwo", "postgresddl:grantOnFunctionStatement");
+            assertNotNull(grantNode);
+            Node parameter_1 = findNode(grantNode, "numerator", "postgresddl:functionParameter");
+            assertNotNull(parameter_1);
+            verifySingleValueProperty(parameter_1, "ddl:datatypeName", "int");
         }
     }
 }
