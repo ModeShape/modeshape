86ddfd1e1c125167c38754928a98f53069cea8ff
Horia Chiorean <hchiorea@redhat.com>
2012-02-08 12:58:06 +0200

    MODE 1343 - Ported zip sequencer to 3.x

---
+++ sequencers/modeshape-sequencer-zip/src/main/java/org/modeshape/sequencer/zip/ZipSequencer.java
@@ -0,0 +1,168 @@
+/*
+ * ModeShape (http://www.modeshape.org)
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * See the AUTHORS.txt file in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * ModeShape is free software. Unless otherwise indicated, all code in ModeShape
+ * is licensed to you under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * ModeShape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.modeshape.sequencer.zip;
+
+import javax.jcr.Binary;
+import javax.jcr.NamespaceRegistry;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import org.modeshape.common.util.CheckArg;
+import org.modeshape.jcr.api.JcrConstants;
+import org.modeshape.jcr.api.nodetype.NodeTypeManager;
+import org.modeshape.jcr.api.sequencer.Sequencer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * A sequencer that processes and extract the files and folders from ZIP archive files.
+ * 
+ * @author Horia Chiorean
+ */
+public class ZipSequencer extends Sequencer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZipSequencer.class);
+
+    @Override
+    public void initialize( NamespaceRegistry registry,
+                            NodeTypeManager nodeTypeManager ) throws RepositoryException, IOException {
+        super.registerNodeTypes("zip.cnd", nodeTypeManager, true);
+    }
+
+    @Override
+    public boolean execute( Property inputProperty,
+                            Node outputNode,
+                            Context context ) throws Exception {
+        Binary binaryValue = inputProperty.getBinary();
+        CheckArg.isNotNull(binaryValue, "binary");
+
+        ZipInputStream zipInputStream = null;
+        try {
+            zipInputStream = new ZipInputStream(binaryValue.getStream());
+            
+            ZipEntry entry = zipInputStream.getNextEntry();
+
+            outputNode = createTopLevelNode(outputNode);
+            
+            while (entry != null) {
+                entry = sequenceZipEntry(outputNode, context, zipInputStream, entry);
+            }
+        } finally {
+            if (zipInputStream != null) {
+                try {
+                    zipInputStream.close();
+                } catch (Exception e) {
+                    LOGGER.warn("Cannot close zip input stream", e);
+                }
+            }
+        }
+        return true;
+    }
+
+    private Node createTopLevelNode( Node outputNode ) throws RepositoryException {
+        // Create top-level node
+        if (!outputNode.isNew()) {
+            outputNode = outputNode.addNode(ZipLexicon.CONTENT);
+        }
+        outputNode.setPrimaryType(ZipLexicon.FILE);
+        return outputNode;
+    }
+
+    private ZipEntry sequenceZipEntry( Node outputNode,
+                                       Context context,
+                                       ZipInputStream zipInputStream,
+                                       ZipEntry entry ) throws RepositoryException, IOException {
+        Node zipEntryNode = createZipEntryPath(outputNode, entry);
+
+        if (!entry.isDirectory()) {
+            addFileContent(zipInputStream, entry, context, zipEntryNode);
+        }
+        return zipInputStream.getNextEntry();
+    }
+
+    private void addFileContent( ZipInputStream zipInputStream,
+                                 ZipEntry entry,
+                                 Context context,
+                                 Node zipFileNode ) throws RepositoryException, IOException {
+        Node contentNode = zipFileNode.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
+        //on session pre-save the appropriate properties should be set automatically
+        contentNode.addMixin(JcrConstants.MIX_LAST_MODIFIED);
+
+        //set the content bytes
+        byte[] contentBytes = readContent(zipInputStream);
+        org.modeshape.jcr.api.Binary contentBinary = context.valueFactory().createBinary(contentBytes);
+        contentNode.setProperty(JcrConstants.JCR_DATA, contentBinary);        
+
+        // Figure out the mime type ...
+        String mimeType = context.mimeTypeDetector().mimeTypeOf(entry.getName(), contentBinary.getStream());
+        if (mimeType != null) {
+            contentNode.setProperty(JcrConstants.JCR_MIME_TYPE, mimeType);
+        }
+    }
+
+    /**
+     * Reads the content from the {@link ZipInputStream}, making sure it doesn't close the stream.
+     */
+    private byte[] readContent( ZipInputStream zipInputStream ) throws IOException {
+        int bufferLength = 1024;
+        byte[] buffer = new byte[bufferLength];
+        int n;
+        ByteArrayOutputStream baout = new ByteArrayOutputStream();
+        while ((n = zipInputStream.read(buffer, 0, bufferLength)) > -1) {
+            baout.write(buffer, 0, n);
+        }
+        return baout.toByteArray();
+    }
+
+    /**
+     * Creates (if necessary) the path from the {@link Node parentNode} to the {@link ZipEntry zip entry}, based on the name of the
+     * zip entry, which should contain its absolute path.
+     */
+    private Node createZipEntryPath( Node parentNode,
+                                     ZipEntry entry ) throws RepositoryException {
+        Node zipEntryNode = parentNode;
+        String entryName = entry.getName();
+        String[] segments = entryName.split("/");
+        for (int i = 0; i < segments.length; i++) {
+            String segmentName = segments[i];
+            try {
+                zipEntryNode = zipEntryNode.getNode(segmentName);
+            } catch (PathNotFoundException e) {
+                //the path does not exist yet - create it
+                boolean isLastSegment = (i == segments.length - 1);
+                String segmentPrimaryType = isLastSegment ? 
+                        (entry.isDirectory() ? JcrConstants.NT_FOLDER : JcrConstants.NT_FILE)
+                        : JcrConstants.NT_FOLDER;
+                zipEntryNode = zipEntryNode.addNode(segmentName, segmentPrimaryType);
+            }
+        }
+        return zipEntryNode;
+    }
+}
---
+++ sequencers/modeshape-sequencer-zip/src/test/java/org/modeshape/sequencer/zip/ZipSequencerTest.java
@@ -0,0 +1,105 @@
+/*
+ * ModeShape (http://www.modeshape.org)
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * See the AUTHORS.txt file in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * ModeShape is free software. Unless otherwise indicated, all code in ModeShape
+ * is licensed to you under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * ModeShape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.modeshape.sequencer.zip;
+
+import javax.jcr.Binary;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.modeshape.common.util.IoUtil;
+import org.modeshape.jcr.api.JcrConstants;
+import org.modeshape.jcr.api.mimetype.MimeTypeConstants;
+import org.modeshape.jcr.sequencer.AbstractSequencerTest;
+import java.io.IOException;
+
+/**
+ * Unit test for {@link ZipSequencer}
+ *
+ * @author Horia Chiorean
+ */
+public class ZipSequencerTest extends AbstractSequencerTest {
+
+    @Test
+    public void shouldSequenceZip1() throws Exception {
+        String filename = "testzip.zip";
+        createNodeWithContentFromFile(filename, filename);
+
+        Node sequencedZip = getSequencedNode(rootNode, "zip/" + filename);
+        assertNotNull(sequencedZip);
+        assertEquals(ZipLexicon.FILE, sequencedZip.getPrimaryNodeType().getName());
+
+        assertEquals(2, sequencedZip.getNodes().getSize());
+        assertFile(sequencedZip, "test1.txt", "This is a test content of file 1\n");
+
+        Node folder = sequencedZip.getNode("test subfolder");
+        assertEquals(1, folder.getNodes().getSize());
+        assertEquals(JcrConstants.NT_FOLDER, folder.getPrimaryNodeType().getName());
+
+        assertFile(folder, "test2.txt", "This is a test content of file2\n");
+    }
+
+
+    private void assertFile( Node parentNode,
+                             String relativePath,
+                             String expectedContent ) throws RepositoryException, IOException {
+        Node file = parentNode.getNode(relativePath);
+        assertNotNull(file);
+        assertEquals(1, file.getNodes().getSize());
+        assertEquals(JcrConstants.NT_FILE, file.getPrimaryNodeType().getName());
+        Node fileContent = file.getNode(JcrConstants.JCR_CONTENT);
+        assertNotNull(fileContent);
+        assertEquals(MimeTypeConstants.TEXT_PLAIN, fileContent.getProperty(JcrConstants.JCR_MIME_TYPE).getString());
+        Binary fileData = fileContent.getProperty(JcrConstants.JCR_DATA).getBinary();
+        assertNotNull(fileData);
+        if (expectedContent != null) {
+            assertEquals(expectedContent, IoUtil.read(fileData.getStream()));
+        }
+    }
+
+    @Test
+    public void shouldSequenceZip2() throws Exception {
+        String filename = "test-files.zip";
+        createNodeWithContentFromFile(filename, filename);
+
+        Node sequencedZip = getSequencedNode(rootNode, "zip/" + filename);
+        assertNotNull(sequencedZip);
+        assertEquals(ZipLexicon.FILE, sequencedZip.getPrimaryNodeType().getName());
+
+        // Find the sequenced node ...
+        String path = "/zip/test-files.zip";       
+        assertNode(path + "/MODE-966-fix.patch", JcrConstants.NT_FILE);
+        assertNode(path + "/MODE-966-fix.patch/jcr:content", JcrConstants.NT_RESOURCE);
+        assertNode(path + "/testFolder", JcrConstants.NT_FOLDER);
+        assertNode(path + "/testFolder/MODE-962-fix.patch", JcrConstants.NT_FILE);
+        assertNode(path + "/testFolder/MODE-962-fix.patch/jcr:content", JcrConstants.NT_RESOURCE);
+        assertNode(path + "/testFolder/testInnerFolder", JcrConstants.NT_FOLDER);
+        assertNode(path + "/testFolder/testInnerFolder/MODE-960-fix.patch", JcrConstants.NT_FILE);
+        assertNode(path + "/testFolder/testInnerFolder/MODE-960-fix.patch/jcr:content", JcrConstants.NT_RESOURCE);
+        assertNode(path + "/testFolder/testInnerFolder/MODE-960-fix2.patch", JcrConstants.NT_FILE);
+        assertNode(path + "/testFolder/testInnerFolder/MODE-960-fix2.patch/jcr:content", JcrConstants.NT_RESOURCE);      
+    }
+}
