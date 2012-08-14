8e88228848159c06e3d53b7dc16631e3cbbb7908
Oleg Kulikov <oleg.kulikoff@gmail.com>
2012-08-14 10:53:20 +0400

    Remove char from type declaration

--- modeshape-jcr/src/test/java/org/modeshape/jcr/value/binary/DatabaseBinaryStoreTest.java
+++ modeshape-jcr/src/test/java/org/modeshape/jcr/value/binary/DatabaseBinaryStoreTest.java
@@ -6,15 +6,19 @@ package org.modeshape.jcr.value.binary;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.modeshape.jcr.store.DataSourceConfig;
+import org.modeshape.jcr.value.BinaryKey;
 import org.modeshape.jcr.value.BinaryValue;
 
 /**
@@ -91,6 +95,36 @@ public class DatabaseBinaryStoreTest {
 
     @Test
     public void shouldMarkAsUnused() throws Exception {
+        String testMessage = "This is a test message";
+        InputStream content = genContent(testMessage);
+
+        BinaryValue ref = store.storeValue(content);
+
+        //checking that it was succesfuly stored
+        InputStream is = store.getInputStream(ref.getKey());
+        assertThat("Failure to get stored value", is, is(notNullValue()));
+
+        //convert content into string for checking
+        byte[] data = read(is);
+        String msg = new String(data, 0, data.length);
+
+        assertThat("Content distortion", msg, is(testMessage));
+
+
+        //put into garbage and mark as unused
+        ArrayList<BinaryKey> garbage = new ArrayList();
+        garbage.add(ref.getKey());
+        store.markAsUnused(garbage);
+
+        //check content avaiability again
+        is = store.getInputStream(ref.getKey());
+        assertThat("Content still available", is, is(nullValue()));
+
+        //allow to expire
+        Thread.sleep(500);
+
+        //removing expired value
+        store.removeValuesUnusedLongerThan(300, TimeUnit.MILLISECONDS);
     }
 
     @Test
@@ -109,4 +143,7 @@ public class DatabaseBinaryStoreTest {
         assertThat(store.getExtractedText(bValue), is("hint"));
     }
 
+    private InputStream genContent(String content) {
+        return new ByteArrayInputStream(content.getBytes());
+    }
 }
