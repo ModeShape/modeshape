f19ee1cad3e87a5eed68f40f59eddb4a783ba1c7
Horia Chiorean <hchiorea@redhat.com>
2012-10-23 14:55:47 +0300

    MODE-1687 - Fixed the registration of unused namespaces when initial cnd files are configured in the repository

--- modeshape-jcr/src/main/java/org/modeshape/jcr/NodeTypesImporter.java
+++ modeshape-jcr/src/main/java/org/modeshape/jcr/NodeTypesImporter.java
@@ -28,12 +28,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.NodeTypeDefinition;
 import org.modeshape.common.collection.Problems;
 import org.modeshape.common.collection.SimpleProblems;
 import org.modeshape.common.logging.Logger;
+import org.modeshape.jcr.value.NamespaceRegistry;
 
 /**
  * Class which performs the import of the optional, repository configured node-types
@@ -59,39 +62,59 @@ public final class NodeTypesImporter {
         }
 
         List<NodeTypeDefinition> nodeTypeDefinitions = new ArrayList<NodeTypeDefinition>();
+        Set<NamespaceRegistry.Namespace> namespaces  = new HashSet<NamespaceRegistry.Namespace>();
         for (String cndFile : nodeTypesFiles) {
-            nodeTypeDefinitions.addAll(parseCNDFile(cndFile));
+            CndImportOperation cndImportOperation = new CndImportOperation();
+            cndImportOperation.execute(cndFile);
+
+            nodeTypeDefinitions.addAll(cndImportOperation.getNodeTypeDefinitions());
+            namespaces.addAll(cndImportOperation.getNamespaces());
         }
 
-        if (nodeTypeDefinitions.isEmpty()) {
-            return;
+        if (!nodeTypeDefinitions.isEmpty()) {
+            runningState.nodeTypeManager().registerNodeTypes(nodeTypeDefinitions, false, false, true);
         }
 
-        runningState.nodeTypeManager().registerNodeTypes(nodeTypeDefinitions, false, false, true);
+        if (!namespaces.isEmpty()) {
+            runningState.persistentRegistry().register(namespaces);
+        }
     }
 
-    private List<NodeTypeDefinition> parseCNDFile( String cndFile ) {
-        InputStream cndFileStream = runningState.environment()
-                                                .getClassLoader(NodeTypesImporter.class.getClassLoader())
-                                                .getResourceAsStream(cndFile);
+    private final class CndImportOperation {
+        private List<NodeTypeDefinition> nodeTypeDefinitions = Collections.emptyList();
+        private Set<NamespaceRegistry.Namespace> namespaces = Collections.emptySet();
 
-        if (cndFileStream == null) {
-            LOGGER.warn(JcrI18n.cannotLoadCndFile, cndFile);
-            return Collections.emptyList();
+        void execute( String cndFile ) {
+            InputStream cndFileStream = runningState.environment()
+                                                    .getClassLoader(NodeTypesImporter.class.getClassLoader())
+                                                    .getResourceAsStream(cndFile);
+
+            if (cndFileStream == null) {
+                LOGGER.warn(JcrI18n.cannotLoadCndFile, cndFile);
+                return;
+            }
+
+            CndImporter cndImporter = new CndImporter(runningState.context(), true);
+            Problems importProblems = new SimpleProblems();
+            try {
+                cndImporter.importFrom(cndFileStream, importProblems, cndFile);
+                if (importProblems.hasErrors()) {
+                    importProblems.writeTo(LOGGER);
+                    return;
+                }
+                this.nodeTypeDefinitions = cndImporter.getNodeTypeDefinitions();
+                this.namespaces = cndImporter.getNamespaces();
+            } catch (IOException e) {
+                LOGGER.error(e, JcrI18n.errorReadingCndFile, cndFile);
+            }
         }
 
-        CndImporter cndImporter = new CndImporter(runningState.context(), true);
-        Problems importProblems = new SimpleProblems();
-        try {
-            cndImporter.importFrom(cndFileStream, importProblems, cndFile);
-            if (importProblems.hasErrors()) {
-                importProblems.writeTo(LOGGER);
-                return Collections.emptyList();
-            }
-            return cndImporter.getNodeTypeDefinitions();
-        } catch (IOException e) {
-            LOGGER.error(e, JcrI18n.errorReadingCndFile, cndFile);
-            return Collections.emptyList();
+        Set<NamespaceRegistry.Namespace> getNamespaces() {
+            return namespaces;
+        }
+
+        List<NodeTypeDefinition> getNodeTypeDefinitions() {
+            return nodeTypeDefinitions;
         }
     }
 }
