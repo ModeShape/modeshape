e86cf8a92eef365c3021fdf72870516ec763d931
Oleg Kulikov <oleg.kulikoff@gmail.com>
2012-11-26 17:39:03 +0300

    MODE-1499: Fix NullPointerException during shutdown process

--- modeshape-jca/ra-jar/src/main/java/org/modeshape/jcr/jca/JcrResourceAdapter.java
+++ modeshape-jca/ra-jar/src/main/java/org/modeshape/jcr/jca/JcrResourceAdapter.java
@@ -69,6 +69,9 @@ public class JcrResourceAdapter implements ResourceAdapter, Serializable {
 
     @Override
     public void stop() {
+        if (engine == null) {
+            return;
+        }
         //shutdown repositories
         for (String name : repositories.keySet()) {
             try {
