f21e4164b2264a804aac466d23f9d5106738243f
Randall Hauch <rhauch@gmail.com>
2012-12-18 13:22:00 -0600

    MODE-1733 Added several concurrency tests that use persistent caches
    None of the tests fail at this point.

--- modeshape-jcr/src/test/java/org/modeshape/jcr/ClusteredRepositoryTest.java
+++ modeshape-jcr/src/test/java/org/modeshape/jcr/ClusteredRepositoryTest.java
@@ -24,25 +24,31 @@
 
 package org.modeshape.jcr;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.EventListener;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.modeshape.common.FixFor;
+import org.modeshape.common.util.FileUtil;
 import org.modeshape.jcr.api.observation.Event;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for various clustered repository scenarios.
- *
+ * 
  * @author Horia Chiorean (hchiorea@redhat.com)
  */
 public class ClusteredRepositoryTest extends AbstractTransactionalTest {
@@ -58,7 +64,7 @@ public class ClusteredRepositoryTest extends AbstractTransactionalTest {
     }
 
     @Test
-    @FixFor("MODE-1618")
+    @FixFor( "MODE-1618" )
     public void shouldPropagateNodeChangesInCluster() throws Exception {
         JcrRepository repository1 = TestingUtil.startRepositoryWithConfig("config/clustered-repo-config.json");
         JcrSession session1 = repository1.login();
@@ -94,10 +100,81 @@ public class ClusteredRepositoryTest extends AbstractTransactionalTest {
         assertEquals(ModeShapeEngine.State.RUNNING, repository.getState());
     }
 
-    @Test(expected = RepositoryException.class)
+    @Test( expected = RepositoryException.class )
     @FixFor( "MODE-1701" )
     public void shouldNotStartRepositoryWithInvalidJGroupsConfiguration() throws Exception {
         TestingUtil.startRepositoryWithConfig("config/clustered-repo-config-invalid-jgroups-file.json");
+    }
+
+    /**
+     * Each Infinispan configuration persists data in a separate location, and we use replication mode.
+     * 
+     * @throws Exception
+     */
+    @Test
+    @FixFor( "MODE-1733" )
+    public void shouldStartClusterWithReplicatedCachePersistedToSeparateAreasForEachProcess() throws Exception {
+        FileUtil.delete("target/clustered");
+        JcrRepository repository1 = null;
+        JcrRepository repository2 = null;
+        try {
+            // Start the first process completely ...
+            repository1 = TestingUtil.startRepositoryWithConfig("config/repo-config-clustered-persistent-1.json");
+            Session session1 = repository1.login();
+            assertThat(session1.getRootNode(), is(notNullValue()));
+            assertThat(session1.getNode("/Cars"), is(notNullValue()));
+            assertThat(session1.getNode("/Cars/Hybrid"), is(notNullValue()));
+            assertThat(session1.getNode("/Cars/Hybrid/Toyota Prius"), is(notNullValue()));
+            assertThat(session1.getWorkspace().getNodeTypeManager().getNodeType("car:Car"), is(notNullValue()));
+            assertThat(session1.getWorkspace().getNodeTypeManager().getNodeType("air:Aircraft"), is(notNullValue()));
+
+            // Start the second process completely ...
+            repository2 = TestingUtil.startRepositoryWithConfig("config/repo-config-clustered-persistent-2.json");
+            Session session2 = repository2.login();
+            assertThat(session2.getRootNode(), is(notNullValue()));
+            assertThat(session2.getNode("/Cars/Hybrid/Toyota Prius"), is(notNullValue()));
+            assertThat(session2.getWorkspace().getNodeTypeManager().getNodeType("car:Car"), is(notNullValue()));
+            assertThat(session2.getWorkspace().getNodeTypeManager().getNodeType("air:Aircraft"), is(notNullValue()));
+
+            session1.logout();
+            session2.logout();
+        } finally {
+            TestingUtil.killRepositories(repository1, repository2);
+            FileUtil.delete("target/clustered");
+        }
+
+    }
+
+    /**
+     * Each Infinispan configuration persists data to the SAME location, including indexes. This is NOT a valid option because the
+     * indexes get corrupted, so we will ignore this
+     * 
+     * @throws Exception
+     */
+    @Ignore
+    @Test
+    @FixFor( "MODE-1733" )
+    public void shouldStartClusterWithReplicatedCachePersistedToSameAreaForBothProcesses() throws Exception {
+        FileUtil.delete("target/clustered");
+        JcrRepository repository1 = null;
+        JcrRepository repository2 = null;
+        try {
+            // Start the first process completely ...
+            repository1 = TestingUtil.startRepositoryWithConfig("config/repo-config-clustered-persistent-1.json");
+            Session session1 = repository1.login();
+            assertThat(session1.getRootNode(), is(notNullValue()));
+
+            // Start the second process completely ...
+            repository2 = TestingUtil.startRepositoryWithConfig("config/repo-config-clustered-persistent-1.json");
+            Session session2 = repository2.login();
+            assertThat(session2.getRootNode(), is(notNullValue()));
+
+            session1.logout();
+            session2.logout();
+        } finally {
+            TestingUtil.killRepositories(repository1, repository2);
+            FileUtil.delete("target/clustered");
+        }
     }
 
     protected class ClusteringEventListener implements EventListener {
