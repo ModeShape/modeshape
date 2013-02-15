95b4bf2a4b742618e7bb5f4871ea6539c3df5d5a
Randall Hauch <rhauch@gmail.com>
2013-02-15 10:37:17 -0600

    MODE-1816 REST v2 responses base property representations on number of values
    The response should base the field value for a given property based upon
    whether that JCR property is single-valued or multi-valued, not upon
    the number of values. If multi-valued, then the field value should be
    an array (even if there is just one item). Version 1 of the REST API
    already works this way, but the version 2 was changed.
    
    Note that it is still possible for property representations in
    requests to use a single value (rather than an array) when setting
    a multi-value to a single value, or the more conventional practice
    of using an array to set a multi-valued array. The latter is obviously
    required when setting the mutli-valued property to 0 or 2+ values.
    
    Several of the expected results were incorrect and have been changed.

--- web/modeshape-web-jcr-rest/src/main/java/org/modeshape/web/jcr/rest/model/RestProperty.java
+++ web/modeshape-web-jcr-rest/src/main/java/org/modeshape/web/jcr/rest/model/RestProperty.java
@@ -24,34 +24,38 @@
 
 package org.modeshape.web.jcr.rest.model;
 
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import java.util.Collections;
 import java.util.List;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 /**
  * A REST representation of the {@link javax.jcr.Property}
- *
+ * 
  * @author Horia Chiorean (hchiorea@redhat.com)
  */
 public final class RestProperty extends RestItem {
 
     private final List<String> values;
+    private final boolean multiValued;
 
     /**
      * Creates a new rest property instance.
-     *
+     * 
      * @param name a {@code non-null} string, the name of the property
      * @param url a {@code non-null} string, the absolute url to this property
      * @param parentUrl a {@code non-null} string, the absolute url to this property's parent
      * @param values a list of possible values for the property, may be {@code null}
+     * @param multiValued true if this property is a multi-valued property
      */
     public RestProperty( String name,
                          String url,
                          String parentUrl,
-                         List<String> values ) {
+                         List<String> values,
+                         boolean multiValued ) {
         super(name, url, parentUrl);
         this.values = values != null ? values : Collections.<String>emptyList();
+        this.multiValued = multiValued;
     }
 
     List<String> getValues() {
@@ -63,7 +67,7 @@ public final class RestProperty extends RestItem {
     }
 
     boolean isMultiValue() {
-        return values.size() > 1;
+        return multiValued;
     }
 
     @Override
