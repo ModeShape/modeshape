f531988277551f32eff23aee25ab91e701254f20
Oleg Kulikov <oleg.kulikoff@gmail.com>
2013-02-25 17:32:34 +0300

    Update test

--- connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/CmisConnectorTest.java
+++ connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/CmisConnectorTest.java
@@ -41,25 +41,25 @@ public class CmisConnectorTest extends AbstractConnectorTest {
     public void before() throws Exception {
     }
 
-//    @Test
+    @Test
     public void shouldAccessRootFolder() throws Exception {
         Node root = getSession().getNode("/cmis");
         assertTrue(root != null);
     }
 
-//    @Test
+    @Test
     public void testRootFolderNodeType() throws Exception {
         Node root = getSession().getNode("/cmis");
         assertEquals("cmis:folder",root.getPrimaryNodeType().getName());
     }
 
-//    @Test
+    @Test
     public void testRootFolderName() throws Exception {
         Node root = getSession().getNode("/cmis");
         assertEquals("cmis", root.getName());
     }
 
-//    @Test
+    @Test
     public void shouldAccessRepositoryInfo() throws Exception {
         Node repoInfo = getSession().getNode("/cmis/repository");
         assertEquals("Dummy cmis repository", repoInfo.getProperty("cmis:productName").getString());
@@ -68,7 +68,7 @@ public class CmisConnectorTest extends AbstractConnectorTest {
     }
 
 
-//    @Test
+    @Test
     public void shouldAccessFolderByPath()  throws Exception {
         Node root = getSession().getNode("/cmis");
         assertTrue(root != null);
@@ -84,13 +84,13 @@ public class CmisConnectorTest extends AbstractConnectorTest {
     }
 
 
- //   @Test
+    @Test
     public void shouldAccessDocumentPath()  throws Exception {
         Node file = getSession().getNode("/cmis/src/pom.xml");
         assertTrue(file != null);
     }
 
-//    @Test
+    @Test
     public void shouldAccessBinaryContent() throws Exception {
         Node file = getSession().getNode("/cmis/src/pom.xml");
         Property value = file.getProperty("cmis:data");
@@ -116,70 +116,70 @@ public class CmisConnectorTest extends AbstractConnectorTest {
     //-----------------------------------------------------------------------/
     // Folder cmis build-in properties
     //-----------------------------------------------------------------------/
- //   @Test
+    @Test
     public void shoudlAccessObjectIdPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String objectId = node.getProperty("cmis:objectId").getString();
         assertTrue(objectId != null);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessNamePropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String name = node.getProperty("cmis:name").getString();
         assertEquals("src", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessBaseTypeIdPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String name = node.getProperty("cmis:baseTypeId").getString();
         assertEquals("cmis:folder", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessObjectTypeIdPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String name = node.getProperty("cmis:objectTypeId").getString();
         assertEquals("cmis:folder", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessCreatedByPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String name = node.getProperty("cmis:createdBy").getString();
         assertEquals("modeshape", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessLastModifiedByPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String name = node.getProperty("cmis:lastModifiedBy").getString();
         assertEquals("modeshape", name);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessCreationDatePropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         Calendar date = node.getProperty("cmis:creationDate").getDate();
         assertTrue(date != null);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessModificationDatePropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         Calendar date = node.getProperty("cmis:lastModificationDate").getDate();
         assertTrue(date != null);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessPathPropertyForFolder() throws Exception {
         Node node = getSession().getNode("/cmis/src");
         String path = node.getProperty("cmis:path").getString();
         assertEquals("/src", path);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessParentIdPropertyForFolder() throws Exception {
         Node root = getSession().getNode("/cmis");
         String rootId = root.getProperty("cmis:objectId").getString();
@@ -192,63 +192,63 @@ public class CmisConnectorTest extends AbstractConnectorTest {
     //-----------------------------------------------------------------------/
     // Document cmis build-in properties
     //-----------------------------------------------------------------------/
-//    @Test
+    @Test
     public void shoudlAccessObjectIdPropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String objectId = node.getProperty("cmis:objectId").getString();
         assertTrue(objectId != null);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessNamePropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String name = node.getProperty("cmis:name").getString();
         assertEquals("pom.xml", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessBaseTypeIdPropertyDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String name = node.getProperty("cmis:baseTypeId").getString();
         assertEquals("cmis:document", name);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessObjectTypeIdPropertyDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String name = node.getProperty("cmis:objectTypeId").getString();
         assertEquals("cmis:document", name);
     }
 
- //   @Test
+    @Test
     public void shoudlAccessCreatedByPropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String name = node.getProperty("cmis:createdBy").getString();
         assertEquals("modeshape", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessLastModifiedByPropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         String name = node.getProperty("cmis:lastModifiedBy").getString();
         assertEquals("modeshape", name);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessCreationDatePropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         Calendar date = node.getProperty("cmis:creationDate").getDate();
         assertTrue(date != null);
     }
 
-//    @Test
+    @Test
     public void shoudlAccessModificationDatePropertyForDocument() throws Exception {
         Node node = getSession().getNode("/cmis/src/pom.xml");
         Calendar date = node.getProperty("cmis:lastModificationDate").getDate();
         assertTrue(date != null);
     }
 
-//    @Test
+    @Test
     public void shouldCreateFolder() throws Exception {
         Node root = getSession().getNode("/cmis/src");
         
@@ -266,13 +266,13 @@ public class CmisConnectorTest extends AbstractConnectorTest {
         node1.setProperty("cmis:data", valueFactory.createBinary(bin));
         node1.setProperty("cmis:mimeType", "text/plain");
 
-//        Node node2 = node.addNode("org", "cmis:folder");
+        Node node2 = node.addNode("org", "cmis:folder");
         getSession().save();
         assertTrue(node != null);
 
     }
 
-//    @Test
+    @Test
     public void shouldCreateDocument() throws Exception {
         System.out.println("Creating document test case:");
         Node root = getSession().getNode("/cmis/src");
