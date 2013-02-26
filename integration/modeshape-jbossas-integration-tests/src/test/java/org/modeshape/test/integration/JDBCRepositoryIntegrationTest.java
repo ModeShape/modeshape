3590e9e82b5458bb0264e4913177955cface33cf
Randall Hauch <rhauch@gmail.com>
2013-02-26 12:35:06 -0600

    MODE-1763 Removed the @Ignore from an integration test
    Removed the @Ignore from the JDBCRepositoryIntegrationTest, which had
    to be added to the changes for MODE-1826 since the fix for
    MODE-1769 has not yet been corrected but will be when Infinispan
    5.2.1.Final is used.

--- integration/modeshape-jbossas-integration-tests/src/test/java/org/modeshape/test/integration/JDBCRepositoryIntegrationTest.java
+++ integration/modeshape-jbossas-integration-tests/src/test/java/org/modeshape/test/integration/JDBCRepositoryIntegrationTest.java
@@ -33,7 +33,6 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
@@ -44,7 +43,6 @@ import static org.junit.Assert.assertNotNull;
  *
  * @author Horia Chiorean (hchiorea@redhat.com)
  */
-@Ignore
 @RunWith( Arquillian.class )
 public class JDBCRepositoryIntegrationTest {
 
