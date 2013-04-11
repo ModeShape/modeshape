3ffad33d30f7f46d5c7c3e5969f1388d9812c8f9
Horia Chiorean <hchiorea@redhat.com>
2013-04-11 16:51:50 +0300

    MODE-1853 - Reverted code introduced by MODE-1729 (commit 2e42871) which shows a similar problem to the class cast exception

--- modeshape-schematic/src/main/java/org/infinispan/schematic/internal/schema/DocumentTransformer.java
+++ modeshape-schematic/src/main/java/org/infinispan/schematic/internal/schema/DocumentTransformer.java
@@ -335,14 +335,15 @@ public class DocumentTransformer {
             LinkedList<Conversion> nestedConversions = entry.getValue();
             Document nested = original.getDocument(segment);
             Document newDoc = convertValuesWithMismatchedTypes(nested, nextLevel, nestedConversions);
-            if (newDoc instanceof DocumentEditor) {
-                // we need to make sure we don't store the converted documents as editors
-                // because if we do, the asserts from DocumentEditor#editable start failing
-                // This can be seen when running the FederationIntegrationTests, which behind the scenes
-                // (in the SourceService class) calls engine.applyChanges()
-                // to update a configuration containing system variables which need replacing
-                changedFields.put(segment, ((DocumentEditor)newDoc).unwrap());
-            }
+            changedFields.put(segment, newDoc);
+//            if (newDoc instanceof DocumentEditor) {
+//                // we need to make sure we don't store the converted documents as editors
+//                // because if we do, the asserts from DocumentEditor#editable start failing
+//                // This can be seen when running the FederationIntegrationTests, which behind the scenes
+//                // (in the SourceService class) calls engine.applyChanges()
+//                // to update a configuration containing system variables which need replacing
+//                changedFields.put(segment, ((DocumentEditor)newDoc).unwrap());
+//            }
         }
 
         // Now create a copy of the original document but with the changed fields ...
