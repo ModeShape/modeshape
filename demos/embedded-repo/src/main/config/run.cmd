595843d6c5d8284cdda97bf890dcb7d9d509aef5
Horia Chiorean <hchiorea@redhat.com>
2013-05-20 15:06:55 +0300

    MODE-1925 - Fixed the packaging and dependency versions of the demos which are part of the main ModeShape distribution.

--- demos/embedded-repo/src/main/config/run.cmd
+++ demos/embedded-repo/src/main/config/run.cmd
@@ -1,1 +1,1 @@
-java -Djava.ext.dirs=./lib -cp modeshape-embedded-repo-demo-${project.version}.jar org.modeshape.demo.embedded.repo.EmbeddedRepositoryDemo
+java -Djava.ext.dirs=./lib -cp  ${project.artifactId}-${project.version}.jar org.modeshape.demo.embedded.repo.EmbeddedRepositoryDemo
--- demos/embedded-repo/src/main/config/run.sh
+++ demos/embedded-repo/src/main/config/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djava.ext.dirs=`pwd`/lib -cp modeshape-embedded-repo-demo-${project.version}.jar org.modeshape.demo.embedded.repo.EmbeddedRepositoryDemo
+java -Djava.ext.dirs=`pwd`/lib -cp  ${project.artifactId}-${project.version}.jar org.modeshape.demo.embedded.repo.EmbeddedRepositoryDemo
