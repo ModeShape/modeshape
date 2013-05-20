3cc3fdc5e73be76a7fe598d534b65b1c1a0150ec
Horia Chiorean <hchiorea@redhat.com>
2013-05-20 15:06:55 +0300

    MODE-1925 - Fixed the packaging and dependency versions of the demos which are part of the main ModeShape distribution.

--- demos/sequencers/src/main/config/run.cmd
+++ demos/sequencers/src/main/config/run.cmd
@@ -1,1 +1,1 @@
-java -Dlogback.configurationFile=logback.xml -Djava.ext.dirs=./lib -cp modeshape-demo-sequencers-${project.version}.jar org.modeshape.demo.sequencer.SequencerDemo %*
+java -Dlogback.configurationFile=logback.xml -Djava.ext.dirs=./lib -cp ${project.artifactId}-${project.version}.jar org.modeshape.demo.sequencer.SequencerDemo
