6329134706deca80dbe12fba08ba1e8b4a525685
Horia Chiorean <hchiorea@redhat.com>
2013-07-23 16:51:53 +0300

    MODE-1707 Updated the EAP & distribution kits to include the JDBC metadata connector

--- integration/modeshape-jbossas-integration-tests/src/test/java/org/modeshape/test/integration/FederationIntegrationTest.java
+++ integration/modeshape-jbossas-integration-tests/src/test/java/org/modeshape/test/integration/FederationIntegrationTest.java
@@ -28,13 +28,16 @@ import java.io.File;
 import javax.annotation.Resource;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.NodeType;
 import javax.jcr.query.Query;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modeshape.connector.meta.jdbc.JdbcMetadataLexicon;
 import org.modeshape.jcr.JcrRepository;
 import org.modeshape.jcr.api.Session;
 import org.modeshape.jcr.api.Workspace;
@@ -42,6 +45,7 @@ import org.modeshape.jcr.api.federation.FederationManager;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * Integration test which verifies that various external sources are correctly set-up via the JBoss AS subsystem.
@@ -58,7 +62,8 @@ public class FederationIntegrationTest {
     public static WebArchive createDeployment() {
         WebArchive archive = ShrinkWrap.create(WebArchive.class, "federatedRepository-test.war");
         // Add our custom Manifest, which has the additional Dependencies entry ...
-        archive.setManifest(new File("src/main/webapp/META-INF/MANIFEST.MF"));
+        archive.setManifest(new File("src/main/webapp/META-INF/MANIFEST.MF"))
+                .addClass(JdbcMetadataLexicon.class);
         return archive;
     }
 
@@ -119,5 +124,26 @@ public class FederationIntegrationTest {
             testRoot.remove();
             session.save();
         }
+    }
+
+    @Test
+    public void shouldHaveJdbcMetadataSourceConfigured() throws Exception {
+        Session defaultSession = repository.login();
+        //predefined
+        Node dbRoot = defaultSession.getNode("/ModeShapeTestDb");
+        assertNotNull(dbRoot);
+
+        assertEquals("nt:unstructured", dbRoot.getPrimaryNodeType().getName());
+        assertNotNull(dbRoot.getProperty(JdbcMetadataLexicon.DATABASE_PRODUCT_NAME.toString()));
+        assertNotNull(dbRoot.getProperty(JdbcMetadataLexicon.DATABASE_PRODUCT_VERSION.toString()));
+        assertNotNull(dbRoot.getProperty(JdbcMetadataLexicon.DATABASE_MAJOR_VERSION.toString()));
+        assertNotNull(dbRoot.getProperty(JdbcMetadataLexicon.DATABASE_MINOR_VERSION.toString()));
+
+        for (NodeType mixin  :dbRoot.getMixinNodeTypes()) {
+            if (mixin.getName().equalsIgnoreCase("mj:databaseRoot")) {
+                return;
+            }
+        }
+        fail("mj:databaseRoot not found on the root database node");
     }
 }
