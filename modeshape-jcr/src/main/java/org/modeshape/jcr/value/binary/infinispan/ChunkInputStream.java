0423ec5a9c34b8061b7c1a74710159c9a7a2f521
Horia Chiorean <hchiorea@redhat.com>
2013-10-14 17:57:56 +0300

    MODE-1752 Updated Infinispan Binary Store's ChunkedInputStream to be able to skip directly to a given chunk, without the need of loading intermediary chunks. This is a backwards compatible change, because all the needed information to perform this operation: the size of 1 chunk, the total size and the number of chunks was already present in the system.

--- modeshape-jcr/src/main/java/org/modeshape/jcr/value/binary/infinispan/ChunkInputStream.java
+++ modeshape-jcr/src/main/java/org/modeshape/jcr/value/binary/infinispan/ChunkInputStream.java
@@ -34,52 +34,69 @@ import java.io.InputStream;
  */
 class ChunkInputStream extends InputStream {
 
-    private final Logger logger;
+    private static final Logger LOGGER = Logger.getLogger(ChunkInputStream.class);
+
     private final Cache<String, byte[]> blobCache;
     private final String key;
+    private final int chunkSize;
+    private final long totalSize;
+    private final int chunksCount;
 
     protected int indexInBuffer;
     protected byte[] buffer;
     private int chunkNumber;
 
-
-    public ChunkInputStream(Cache<String, byte[]> blobCache, String key){
-        logger = Logger.getLogger(getClass());
+    protected ChunkInputStream( Cache<String, byte[]> blobCache,
+                                String key,
+                                int chunkSize,
+                                long totalSize ) {
         this.blobCache = blobCache;
         this.key = key;
+        this.chunkSize = chunkSize;
+        this.totalSize = totalSize;
+        this.chunkNumber = 0;
+        this.indexInBuffer = 0;
+        int remainderSize = (int) (totalSize % chunkSize);
+        int numberOfChunks = (int) totalSize / chunkSize;
+        this.chunksCount = remainderSize > 0 ? numberOfChunks + 1 : numberOfChunks;
     }
 
     @Override
     public int read() throws IOException {
-        if(indexInBuffer == -1){
+        if (indexInBuffer == -1) {
             return -1;
         }
-        if(buffer == null || indexInBuffer >= buffer.length){
-            fillBuffer();
+        if (buffer == null) {
+            fillBufferWithFirstChunk();
+            return read();
+        } else if (indexInBuffer >= buffer.length) {
+            fillBufferWithNextChunk();
             return read();
         }
         return buffer[indexInBuffer++] & 0xff;
     }
 
     @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-        if(indexInBuffer == -1){
+    public int read( byte[] b,
+                     int off,
+                     int len ) throws IOException {
+        if (indexInBuffer == -1) {
             return -1;
         }
-        if(buffer == null){
-            fillBuffer();
+        if (buffer == null) {
+            fillBufferWithFirstChunk();
             return read(b, off, len);
         }
-        if(indexInBuffer >= buffer.length){
+        if (indexInBuffer >= buffer.length) {
             return -1;
         }
-        if (indexInBuffer + len > buffer.length){
+        if (indexInBuffer + len > buffer.length) {
             len = buffer.length - indexInBuffer;
         }
         System.arraycopy(buffer, indexInBuffer, b, off, len);
         indexInBuffer += len;
-        if(indexInBuffer >= buffer.length){
-            fillBuffer();
+        if (indexInBuffer >= buffer.length) {
+            fillBufferWithNextChunk();
         }
         return len;
     }
@@ -87,44 +104,92 @@ class ChunkInputStream extends InputStream {
     @Override
     public int available() {
         if (buffer == null) {
-            fillBuffer();
+            fillBufferWithFirstChunk();
         }
         return buffer.length - indexInBuffer;
     }
 
     @Override
-    public final long skip(long n) throws IOException {
-        if(n <= 0 || indexInBuffer == -1){
+    public final long skip( long n ) throws IOException {
+        if (n <= 0 || indexInBuffer == -1 || totalSize == 0) {
             return 0;
         }
-        if(buffer == null){
-            fillBuffer();
-            return skip(n);
-        }
-        if (buffer.length + n > indexInBuffer){
-            n = buffer.length - indexInBuffer;
-        }
-        if (n < 0){
-            return 0;
-        }
-        indexInBuffer += n;
-        return n;
+        return directSkip(n);
     }
 
-    private void fillBuffer() {
+    @Override
+    public void close() throws IOException {
+        endOfStream();
+    }
 
-        buffer = nextChunk();
-        if(buffer == null){
-            buffer = new byte[0];
-            indexInBuffer = -1;
+    private long directSkip( long n ) {
+        long availableInBuffer = buffer != null ? (buffer.length - indexInBuffer) : chunkSize;
+
+        if (n < availableInBuffer) {
+            //we can skip "n" without requiring any additional chunks
+            if (buffer == null) {
+                //we haven't been initialized yet, so load the first chunk
+                fillBufferWithFirstChunk();
+            }
+            indexInBuffer += n;
+            return n;
+        }
+
+        //we need to skip past the current chunk, so find the chunk which needs to be loaded
+        long lastChunkSize = totalSize - (chunksCount - 1) * chunkSize;
+        int chunksAvailableToSkip = chunksCount - chunkNumber - 1;
+        long bytesAvailableToSkip = (chunksAvailableToSkip - 1) * chunkSize + lastChunkSize;
+
+        long stillRequiredToSkip = n - availableInBuffer;
+        int chunksToSkipOver = (int) (stillRequiredToSkip / chunkSize);
+        int leftToReadAfterSkip = (int) (stillRequiredToSkip % chunkSize);
+        chunkNumber = chunkNumber + chunksToSkipOver + 1;   //chunk# is 0 based
+
+        if (chunkNumber >= chunksCount) {
+            //we would need to skip more chunks than we have
+            endOfStream();
+            return availableInBuffer + bytesAvailableToSkip;
+        }
+        //move directly to the required chunk
+        fillBuffer(chunkNumber);
+        if (buffer.length > leftToReadAfterSkip) {
+            //move the pointer in this chunk
+            indexInBuffer = leftToReadAfterSkip;
+            return n;
+        } else {
+            //we jumped to a valid chunk, but it doesn't have enough data
+            endOfStream();
+            return availableInBuffer + bytesAvailableToSkip;
+        }
+    }
+
+    private void fillBufferWithNextChunk() {
+        this.chunkNumber++;
+        fillBuffer(this.chunkNumber);
+    }
+
+    private void fillBufferWithFirstChunk() {
+        fillBuffer(0);
+    }
+
+    private void fillBuffer(int chunkNumber) {
+        buffer = readChunk(chunkNumber);
+        if (buffer == null) {
+            endOfStream();
         } else {
             indexInBuffer = 0;
         }
     }
 
-    protected byte[] nextChunk() {
-        String chunkKey = key+"-"+chunkNumber++;
-        logger.debug("Read chunk {0}", chunkKey);
+    private void endOfStream() {
+        buffer = new byte[0];
+        indexInBuffer = -1;
+        chunkNumber = -1;
+    }
+
+    private byte[] readChunk( int chunkNumber ) {
+        String chunkKey = key + "-" + chunkNumber;
+        LOGGER.debug("Read chunk {0}", chunkKey);
         return blobCache.get(chunkKey);
     }
 }
