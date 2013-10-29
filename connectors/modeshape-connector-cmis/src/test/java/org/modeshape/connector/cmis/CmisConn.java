d947b5c52872bc437f56940556cebf2af42c7a69
okulikov <oleg.kulikoff@gmail.com>
2013-10-29 16:32:02 +0400

    xxx

---
+++ connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/cmis/CmisConn.java
@@ -0,0 +1,313 @@
+/*
+ * ModeShape (http://www.modeshape.org)
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * See the AUTHORS.txt file in the distribution for a full listing of
+ * individual contributors.
+ *
+ * Unless otherwise indicated, all code in ModeShape is licensed
+ * to you under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * ModeShape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.modeshape.connector.cmis;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import javax.jcr.Binary;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.nodetype.NodeType;
+import javax.jcr.nodetype.NodeTypeIterator;
+import javax.jcr.nodetype.NodeTypeManager;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.client.api.SessionFactory;
+import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.modeshape.jcr.MultiUseAbstractTest;
+import org.modeshape.jcr.RepositoryConfiguration;
+
+/**
+ * Provide integration testing of the CMIS connector with OpenCMIS InMemory Repository.
+ * 
+ * @author Alexander Voloshyn
+ * @version 1.0 2/20/2013
+ */
+public class CmisConnectorIT extends MultiUseAbstractTest {
+    /**
+     * Test OpenCMIS InMemory Server URL.
+     * <p/>
+     * This OpenCMIS InMemory server instance should be started by maven cargo plugin at pre integration stage.
+     */
+    private static final String CMIS_URL = "http://localhost:8090/";
+    private static Logger logger = Logger.getLogger(CmisConnectorIT.class);
+
+    @BeforeClass
+    public static void beforeAll() throws Exception {
+        RepositoryConfiguration config = RepositoryConfiguration.read("config/repository-1.json");
+        startRepository(config);
+
+        // waiting when CMIS repository will be ready
+        boolean isReady = false;
+
+        // max time for waiting in milliseconds
+        long maxTime = 30000L;
+
+        // actially waiting time in milliseconds
+        long waitingTime = 0L;
+
+        // time quant in milliseconds
+        long timeQuant = 500L;
+
+        logger.info("Waiting for CMIS repository...");
+        do {
+            try {
+                testDirectChemistryConnect();
+                isReady = true;
+            } catch (Exception e) {
+                Thread.sleep(timeQuant);
+                waitingTime += timeQuant;
+            }
+        } while (!isReady && waitingTime < maxTime);
+
+        // checking status
+        if (!isReady) {
+            throw new IllegalStateException("CMIS repository did not respond withing " + maxTime + " milliseconds");
+        }
+        logger.info("CMIS repository has been started successfuly");
+    }
+
+    @AfterClass
+    public static void afterAll() throws Exception {
+        MultiUseAbstractTest.afterAll();
+    }
+
+    public static void testDirectChemistryConnect() {
+        // default factory implementation
+        SessionFactory factory = SessionFactoryImpl.newInstance();
+        Map<String, String> parameter = new HashMap<String, String>();
+
+        // connection settings
+        parameter.put(SessionParameter.BINDING_TYPE, BindingType.WEBSERVICES.value());
+        parameter.put(SessionParameter.WEBSERVICES_ACL_SERVICE, CMIS_URL + "services/ACLService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_DISCOVERY_SERVICE, CMIS_URL + "services/DiscoveryService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_MULTIFILING_SERVICE, CMIS_URL + "services/MultiFilingService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_NAVIGATION_SERVICE, CMIS_URL + "services/NavigationService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_OBJECT_SERVICE, CMIS_URL + "services/ObjectService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_POLICY_SERVICE, CMIS_URL + "services/PolicyService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_RELATIONSHIP_SERVICE, CMIS_URL + "services/RelationshipService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_REPOSITORY_SERVICE, CMIS_URL + "services/RepositoryService?wsdl");
+        parameter.put(SessionParameter.WEBSERVICES_VERSIONING_SERVICE, CMIS_URL + "services/VersioningService?wsdl");
+        // Default repository id for in memory server is A1
+        parameter.put(SessionParameter.REPOSITORY_ID, "A1");
+
+        // create session
+        final Session session = factory.createSession(parameter);
+        assertTrue("Chemistry session should exists.", session != null);
+    }
+
+    @Test
+    public void shouldSeeCmisTypesAsJcrTypes() throws Exception {
+        NodeTypeManager manager = getSession().getWorkspace().getNodeTypeManager();
+
+        NodeTypeIterator it = manager.getNodeType("nt:file").getDeclaredSubtypes();
+        while (it.hasNext()) {
+            NodeType nodeType = it.nextNodeType();
+            assertTrue(nodeType != null);
+        }
+    }
+
+    @Test
+    public void shouldAccessRootFolder() throws Exception {
+        Node root = getSession().getNode("/cmis");
+        assertTrue(root != null);
+    }
+
+    @Test
+    public void testRootFolderName() throws Exception {
+        Node root = getSession().getNode("/cmis");
+        assertEquals("cmis", root.getName());
+    }
+
+    @Test
+    public void shouldAccessRepositoryInfo() throws Exception {
+        Node repoInfo = getSession().getNode("/cmis/repositoryInfo");
+        // Different Chemistry versions return different things ...
+        assertTrue(repoInfo.getProperty("cmis:productName").getString().contains("OpenCMIS"));
+        assertTrue(repoInfo.getProperty("cmis:productName").getString().contains("InMemory"));
+        assertEquals("Apache Chemistry", repoInfo.getProperty("cmis:vendorName").getString());
+        assertTrue(repoInfo.getProperty("cmis:productVersion").getString() != null);
+    }
+
+    @Test
+    public void shouldAccessFolderByPath() throws Exception {
+        Node root = getSession().getNode("/cmis");
+        assertTrue(root != null);
+
+        Node node1 = getSession().getNode("/cmis/My_Folder-0-0");
+        assertTrue(node1 != null);
+
+        Node node2 = getSession().getNode("/cmis/My_Folder-0-0/My_Folder-1-0");
+        assertTrue(node2 != null);
+
+        Node node3 = getSession().getNode("/cmis/My_Folder-0-0/My_Folder-1-0/My_Folder-2-0");
+        assertTrue(node3 != null);
+    }
+
+    @Test
+    public void shouldAccessDocumentPath() throws Exception {
+        Node file = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        assertTrue(file != null);
+    }
+
+    @Test
+    public void shouldAccessBinaryContent() throws Exception {
+        Node file = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        Node cnt = file.getNode("jcr:content");
+
+        Property value = cnt.getProperty("jcr:data");
+
+        Binary bv = value.getValue().getBinary();
+        InputStream is = bv.getStream();
+
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        int b = 0;
+        while (b != -1) {
+            b = is.read();
+            if (b != -1) {
+                bout.write(b);
+            }
+        }
+
+        byte[] content = bout.toByteArray();
+        String s = new String(content, 0, content.length);
+
+        assertFalse("Content shouldn't be empty.", s.trim().isEmpty());
+    }
+
+    // -----------------------------------------------------------------------/
+    // Folder cmis build-in properties
+    // -----------------------------------------------------------------------/
+    @Test
+    public void shouldAccessObjectIdPropertyForFolder() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0");
+        String objectId = node.getProperty("jcr:uuid").getString();
+        assertTrue(objectId != null);
+    }
+
+    @Test
+    public void shouldAccessNamePropertyForFolder() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0");
+        String name = node.getName();
+        assertEquals("My_Folder-0-0", name);
+    }
+
+    @Test
+    public void shouldAccessCreatedByPropertyForFolder() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0");
+        String name = node.getProperty("jcr:createdBy").getString();
+        assertEquals("unknown", name);
+    }
+
+    @Test
+    public void shouldAccessCreationDatePropertyForFolder() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0");
+        Calendar date = node.getProperty("jcr:created").getDate();
+        assertTrue(date != null);
+    }
+
+    // @Test
+    public void shouldAccessModificationDatePropertyForFolder() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0");
+        Calendar date = node.getProperty("jcr:lastModified").getDate();
+        assertTrue(date != null);
+    }
+
+    // -----------------------------------------------------------------------/
+    // Document cmis build-in properties
+    // -----------------------------------------------------------------------/
+    @Test
+    public void shouldAccessObjectIdPropertyForDocument() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        String objectId = node.getProperty("jcr:uuid").getString();
+        assertTrue(objectId != null);
+    }
+
+    @Test
+    public void shouldAccessCreatedByPropertyForDocument() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        String name = node.getProperty("jcr:createdBy").getString();
+        assertEquals("unknown", name);
+    }
+
+    @Test
+    public void shouldAccessCreationDatePropertyForDocument() throws Exception {
+        Node node = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        Calendar date = node.getProperty("jcr:created").getDate();
+        assertTrue(date != null);
+    }
+
+    @Test
+    public void shouldCreateFolderAndDocument() throws Exception {
+        Node root = getSession().getNode("/cmis");
+
+        String name = "test" + System.currentTimeMillis();
+        Node node = root.addNode(name, "nt:folder");
+        assertTrue(name.equals(node.getName()));
+        // node.setProperty("name", "test-name");
+
+        root = getSession().getNode("/cmis/" + name);
+        Node node1 = root.addNode("test-1", "nt:file");
+        // System.out.println("Test: creating binary content");
+        byte[] content = "Hello World".getBytes();
+        ByteArrayInputStream bin = new ByteArrayInputStream(content);
+        bin.reset();
+
+        // System.out.println("Test: creating content node");
+        Node contentNode = node1.addNode("jcr:content", "nt:resource");
+        Binary binary = session.getValueFactory().createBinary(bin);
+        contentNode.setProperty("jcr:data", binary);
+        contentNode.setProperty("jcr:lastModified", Calendar.getInstance());
+
+        // System.out.println("Test: trying to save");
+        getSession().save();
+        // System.out.println("Test: checking result");
+
+    }
+
+    @Test
+    public void shouldModifyDocument() throws Exception {
+        Node file = getSession().getNode("/cmis/My_Folder-0-0/My_Document-1-0");
+        PropertyIterator it = file.getProperties();
+        while (it.hasNext()) {
+            Object val = it.nextProperty();
+            printMessage("property=>" + val);
+        }
+        file.setProperty("StringProp", "modeshape");
+        getSession().save();
+    }
+}
---
+++ modeshape-jcr/src/test/java/org/modeshape/jcr/MoveNodeTest.java
@@ -0,0 +1,396 @@
+/*
+ * ModeShape (http://www.modeshape.org)
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ * See the AUTHORS.txt file in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * ModeShape is free software. Unless otherwise indicated, all code in ModeShape
+ * is licensed to you under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * ModeShape is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.modeshape.jcr.security;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import java.security.AccessControlException;
+import javax.jcr.AccessDeniedException;
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.security.AccessControlList;
+import javax.jcr.security.AccessControlManager;
+import javax.jcr.security.AccessControlPolicyIterator;
+import javax.jcr.security.Privilege;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsNull;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.modeshape.common.FixFor;
+import org.modeshape.jcr.MultiUseAbstractTest;
+import org.modeshape.jcr.security.acl.Privileges;
+
+public class AccessControlManagerTest extends MultiUseAbstractTest {
+
+    private AccessControlManager acm;
+    private Privileges privileges;
+
+    @BeforeClass
+    public static final void beforeAll() throws Exception {
+        MultiUseAbstractTest.beforeAll();
+
+        // Import the node types and the data ...
+        registerNodeTypes("cars.cnd");
+        importContent("/", "io/cars-system-view-with-uuids.xml", ImportUUIDBehavior.IMPORT_UUID_COLLISION_THROW);
+
+        setPolicy("/Cars/Luxury/Cadillac DTS", Privilege.JCR_READ, Privilege.JCR_WRITE, Privilege.JCR_MODIFY_ACCESS_CONTROL);
+        setPolicy("/Cars/Luxury/", Privilege.JCR_READ, Privilege.JCR_MODIFY_ACCESS_CONTROL);
+        setPolicy("/Cars/Sports/", Privilege.JCR_READ, Privilege.JCR_WRITE, Privilege.JCR_MODIFY_ACCESS_CONTROL);
+        setPolicy("/Cars/Utility/Ford F-150/", Privilege.JCR_MODIFY_ACCESS_CONTROL, Privilege.JCR_READ_ACCESS_CONTROL);
+        setPolicy("/Cars/Utility/", Privilege.JCR_READ_ACCESS_CONTROL);
+
+    }
+
+    @AfterClass
+    public static final void afterAll() throws Exception {
+        MultiUseAbstractTest.afterAll();
+    }
+
+    @Override
+    @Before
+    public void beforeEach() throws Exception {
+        super.beforeEach();
+        acm = session.getAccessControlManager();
+        privileges = new Privileges(session);
+    }
+
+    @Test
+    public void testSecondSession() throws Exception {
+        Session session2 = session.getRepository().login();
+        session2.logout();
+    }
+
+    @Test
+    public void shouldObtainAccessControlManager() throws Exception {
+        assertTrue(acm != null);
+        Matcher<AccessControlManager> m = IsNull.notNullValue();
+        m.matches(session.getAccessControlManager());
+    }
+
+    @Test
+    public void testGetSupportedPrivileges() throws Exception {
+        Privilege[] permissions = acm.getSupportedPrivileges("/");
+        assertEquals(privileges.listOfSupported().length, permissions.length);
+    }
+
+    @Test
+    public void testPrivilegeForName() throws Exception {
+        Privilege p = acm.privilegeFromName(Privilege.JCR_ALL);
+        assertEquals("jcr:all", p.getName());
+    }
+
+    // --------------- Testing access list -------------------------------------/
+    @Test
+    public void shouldHaveReadPrivilege() throws Exception {
+        Privilege[] privileges = acm.getPrivileges("/Cars/Luxury");
+        assertEquals("jcr:read", privileges[0].getName());
+    }
+
+    @Test
+    public void shoudlHaveReadWritePrivilege() throws Exception {
+        Privilege[] privileges = acm.getPrivileges("/Cars/Luxury/Cadillac DTS");
+        assertTrue(contains("jcr:read", privileges));
+        assertTrue(contains("jcr:write", privileges));
+    }
+
+    @Test
+    public void shoudlDeriveAccessList() throws Exception {
+        Privilege[] privileges = acm.getPrivileges("/Cars/Luxury/Lexus IS350");
+        assertEquals("jcr:read", privileges[0].getName());
+    }
+
+    @Test
+    public void shoudlGrantAllPermissions() throws Exception {
+        Privilege[] privileges = acm.getPrivileges("/Cars/Hybrid");
+        assertTrue(contains("jcr:all", privileges));
+    }
+
+    @Test
+    public void shouldGrantAdd() throws Exception {
+        Node sports = session.getNode("/Cars/Sports");
+        try {
+            sports.addNode("Chevrolet Camaro", "car:Car");
+        } catch (AccessDeniedException e) {
+            fail("Should grant add");
+        }
+    }
+
+    @Test
+    public void shouldDenyAdd() throws RepositoryException {
+        Node luxury = session.getNode("/Cars/Luxury");
+        try {
+            luxury.addNode("Cadillac Flitwood", "car:Car");
+            fail("Should deny add node");
+        } catch (AccessDeniedException e) {
+            System.out.println("Hide exception");
+        } catch (AccessControlException e) {
+            System.out.println("Hide exception");
+        }
+    }
+
+    @Test
+    public void shouldGrantModify() throws RepositoryException {
+        Node infinity = session.getNode("/Cars/Sports/Infiniti G37");
+        try {
+            infinity.setProperty("car:msrp", "$34,901");
+        } catch (AccessDeniedException e) {
+            fail("Should grant modification");
+        }
+    }
+
+    @Test
+    public void shouldDenyModify() throws RepositoryException {
+        Node car = session.getNode("/Cars/Luxury/Lexus IS350");
+        try {
+            car.setProperty("car:msrp", "$34,901");
+            fail("Should deny modification");
+        } catch (AccessDeniedException e) {
+        }
+    }
+
+    @Test
+    public void shouldGrantRemove() throws RepositoryException {
+        Node car = session.getNode("/Cars/Sports/Infiniti G37");
+        try {
+            car.remove();
+        } catch (AccessDeniedException e) {
+            fail("Should grant remove operation");
+        }
+    }
+
+    @Test
+    public void shouldDenyRemove() throws RepositoryException {
+        Node car = session.getNode("/Cars/Luxury/Lexus IS350");
+        try {
+            car.remove();
+            fail("Should deny remove operation");
+        } catch (AccessDeniedException e) {
+        } catch (AccessControlException e) {
+        }
+    }
+
+    @Test
+    public void shoudlDenyRemove2() throws RepositoryException {
+        Node car = session.getNode("/Cars/Luxury/Cadillac DTS");
+        try {
+            car.remove();
+            fail("Should deny remove operation: Parent node has no privilege to remove child node");
+        } catch (AccessDeniedException e) {
+        } catch (AccessControlException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldAllowSetPolicy() throws RepositoryException {
+        setPolicy("/Cars/Utility/Ford F-150", Privilege.JCR_ALL);
+    }
+
+    @Test
+    public void shouldDenySetPolicy() throws RepositoryException {
+        try {
+            setPolicy("/Cars/Utility", Privilege.JCR_ALL);
+            fail("Should deny access list modification");
+        } catch (AccessDeniedException e) {
+        }
+    }
+
+    // @Test
+    public void shouldRemovePolicy() throws RepositoryException {
+        acm.removePolicy("/Cars/Utility/Ford F-150", null);
+        Privilege[] privileges = acm.getPrivileges("/Cars/Utility/Ford F-150");
+        assertEquals(Privilege.JCR_ALL, privileges[0].getName());
+    }
+
+    // -------------------- Testing access control api ---
+    @Test
+    public void onlyAccessControlAPIAllowsRemoveACL() throws Exception {
+        Node node = session.getNode("/Cars/Luxury/mode:acl");
+        assertThat(node, is(notNullValue()));
+        try {
+            node.remove();
+            fail("Only Access Control API allows modification");
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void onlyAccessControlAPIAllowsAddACL() throws Exception {
+        Node node = session.getNode("/Cars/Hybrid");
+        assertThat(node, is(notNullValue()));
+        try {
+            node.addMixin("mix:accessControllable");
+            Node acl = node.addNode("mode:acl", "mode:Acl");
+            acl.addNode("test", "mode:Permission");
+            fail("Only Access Control API allows modification");
+        } catch (RepositoryException e) {
+        }
+    }
+
+    @Test
+    public void shouldNotDependFromContentPermissions() throws Exception {
+        setPolicy("/Cars/Luxury/Bentley Continental", Privilege.JCR_WRITE);
+    }
+
+    @Test
+    @FixFor( "MODE-2036" )
+    public void shouldDenyAccessChildNode() throws Exception {
+        Node root = session.getRootNode();
+        Node truks = root.addNode("truks");
+        session.save();
+
+        AccessControlManager acm = session.getAccessControlManager();
+        Privilege[] privileges = new Privilege[] {acm.privilegeFromName(Privilege.JCR_ALL)};
+
+        AccessControlList acl;
+        AccessControlPolicyIterator it = acm.getApplicablePolicies(truks.getPath());
+        if (it.hasNext()) {
+            acl = (AccessControlList)it.nextAccessControlPolicy();
+        } else {
+            acl = (AccessControlList)acm.getPolicies(truks.getPath())[0];
+        }
+        acl.addAccessControlEntry(SimplePrincipal.newInstance("Admin"), privileges);
+
+        acm.setPolicy(truks.getPath(), acl);
+        session.save();
+
+        try {
+            root.getNode("truks");
+            fail("Access list should deny access");
+        } catch (AccessControlException e) {
+        }
+    }
+
+    @Test
+    public void shouldAllowAccessUsingRole() throws Exception {
+        Node root = session.getRootNode();
+        Node truks = root.addNode("tractors");
+        session.save();
+
+        AccessControlManager acm = session.getAccessControlManager();
+        Privilege[] privileges = new Privilege[] {acm.privilegeFromName(Privilege.JCR_ALL)};
+
+        AccessControlList acl;
+        AccessControlPolicyIterator it = acm.getApplicablePolicies(truks.getPath());
+        if (it.hasNext()) {
+            acl = (AccessControlList)it.nextAccessControlPolicy();
+        } else {
+            acl = (AccessControlList)acm.getPolicies(truks.getPath())[0];
+        }
+        acl.addAccessControlEntry(SimplePrincipal.newInstance("admin"), privileges);
+
+        acm.setPolicy(truks.getPath(), acl);
+        session.save();
+
+        Node node = root.getNode("tractors");
+        assertThat(node, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldAllowRead() throws Exception {
+        Node root = session.getRootNode();
+        Node aircraft = root.addNode("aircraft");
+        assertThat(aircraft, is(notNullValue()));
+
+        AccessControlList acl2 = getACL("/aircraft");
+        acl2.addAccessControlEntry(SimplePrincipal.newInstance("Admin"),
+                                   new Privilege[] {acm.privilegeFromName(Privilege.JCR_ALL)});
+        acl2.addAccessControlEntry(SimplePrincipal.newInstance("anonymous"),
+                                   new Privilege[] {acm.privilegeFromName(Privilege.JCR_READ)});
+
+        acm.setPolicy("/aircraft", acl2);
+
+        AccessControlList acl = getACL("/");
+        acl.addAccessControlEntry(SimplePrincipal.newInstance("Admin"),
+                                  new Privilege[] {acm.privilegeFromName(Privilege.JCR_ALL)});
+        acl.addAccessControlEntry(SimplePrincipal.newInstance("anonymous"),
+                                  new Privilege[] {acm.privilegeFromName(Privilege.JCR_READ)});
+
+        acm.setPolicy("/", acl);
+
+        session.save();
+
+        root = session.getRootNode();
+        aircraft = root.getNode("aircraft");
+    }
+
+    // -------------------------------
+
+    @Test
+    public void testGetApplicablePolicies() throws Exception {
+        AccessControlList acl = (AccessControlList)acm.getApplicablePolicies("/Cars").nextAccessControlPolicy();
+        assertTrue(acl != null);
+    }
+
+    private static void setPolicy( String path,
+                                   String... privileges ) throws UnsupportedRepositoryOperationException, RepositoryException {
+        AccessControlManager acm = session.getAccessControlManager();
+
+        Privilege[] permissions = new Privilege[privileges.length];
+        for (int i = 0; i < privileges.length; i++) {
+            permissions[i] = acm.privilegeFromName(privileges[i]);
+        }
+
+        AccessControlList acl = null;
+        AccessControlPolicyIterator it = acm.getApplicablePolicies(path);
+        if (it.hasNext()) {
+            acl = (AccessControlList)it.nextAccessControlPolicy();
+        } else {
+            acl = (AccessControlList)acm.getPolicies(path)[0];
+        }
+        acl.addAccessControlEntry(SimplePrincipal.newInstance("anonymous"), permissions);
+
+        acm.setPolicy(path, acl);
+        session.save();
+    }
+
+    private AccessControlList getACL( String path ) throws Exception {
+        AccessControlPolicyIterator it = acm.getApplicablePolicies(path);
+        if (it.hasNext()) {
+            return (AccessControlList)it.nextAccessControlPolicy();
+        }
+        return (AccessControlList)acm.getPolicies(path)[0];
+    }
+
+    private boolean contains( String name,
+                              Privilege[] privileges ) {
+        for (int i = 0; i < privileges.length; i++) {
+            if (name.equals(privileges[i].getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
