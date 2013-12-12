4332b74a5f0389d421834d75b7e7eb0314ffffb9
Randall Hauch <rhauch@gmail.com>
2013-12-12 16:00:09 -0600

    MODE-2098 Minor JavaDoc improvement/correction.

--- modeshape-jcr-api/src/main/java/org/modeshape/jcr/api/Property.java
+++ modeshape-jcr-api/src/main/java/org/modeshape/jcr/api/Property.java
@@ -35,8 +35,8 @@ import javax.jcr.ValueFormatException;
 public interface Property extends javax.jcr.Property {
 
     /**
-     * Converts the value of the property (if possible) to the specified type.
-     * The list of supported types is:
+     * Converts the value(s) of the property (if possible) to the specified type.
+     * The list of supported types for single-valued properties is:
      * <ul>
      *     <li>{@link String}</li>
      *     <li>{@link Long}</li>
@@ -52,13 +52,56 @@ public interface Property extends javax.jcr.Property {
      *     <li>{@link javax.jcr.Node}</li>
      *     <li>{@link javax.jcr.NodeIterator}</li>
      * </ul>
+     * The list of supported types for multi-valued properties is:
+     * <ul>
+     *     <li>{@link String}{@code []}</li>
+     *     <li>{@link Long}{@code []}</li>
+     *     <li>{@link Double}{@code []}</li>
+     *     <li>{@link Boolean}{@code []}</li>
+     *     <li>{@link java.math.BigDecimal}{@code []}</li>
+     *     <li>{@link java.util.Calendar}{@code []}</li>
+     *     <li>{@link java.util.Date}{@code []}</li>
+     *     <li>{@link org.modeshape.jcr.api.value.DateTime}{@code []}</li>
+     *     <li>{@link org.modeshape.jcr.api.Binary}{@code []}</li>
+     *     <li>{@link javax.jcr.Binary}{@code []}</li>
+     *     <li>{@link java.io.InputStream}{@code []}</li>
+     *     <li>{@link javax.jcr.Node}{@code []}</li>
+     *     <li>{@link javax.jcr.NodeIterator}</li>
+     * </ul>
      * <p>
-     * In case of <b>homogeneous</b> (all values are of the same type) multi-valued properties, the value type should be the
-     * array version of one of the above types, e.g. {@code getAs(Long[].class)}.
-     * The only exception from this rule is that in the case of multi-valued reference properties
-     * a simple {@link javax.jcr.NodeIterator} type is expected: {@code getAs(NodeIterator.class)}.
+     * For single-valued properties, this method attempts to convert the actual value into the specified type.
+     * For multi-valued properties, this method attempts to convert the actual values into an array of the specified 
+     * type; the only exception to this rule is that in the 
+     * case of multi-valued reference properties a simple {@link javax.jcr.NodeIterator} type is expected: 
+     * {@code getAs(NodeIterator.class)}.
+     * </p>
+     * <p>
+     * For example, the following shows how to obtain the value of a single-valued property as a {@code String} value:
+     * <pre>
+     *     Property prop = ...
+     *     String value = prop.getAs(String.class);
+     * </pre>
+     * If the property is multi-valued, then the values can be obtained by passing in the desired array type:
+     * <pre>
+     *     String[] values = prop.getAs(String[].class);
+     * </pre>
+     * This example will always work, since all property values can be converted to a {@code String}.
+     * </p>
+     * <p>
+     * The following attempts to convert all values to {@link java.util.Date} instances:
+     * <pre>
+     *     Date[] values = prop.getAs(Date[].class);
+     * </pre>
+     * </p>
+     * <p>
+     * Finally, a single- or multi-valued property whose value(s) are references can easily be obtained as a
+     * {@link NodeIterator}, which can represent one or multiple {@link Node} instances:
+     * <pre>
+     *     NodeIterator iter = prop.getAs(NodeIterator.class);
+     * </pre>
+     * </p>
      *
-     * @param type a {@link Class} representing the type to which to convert the value of the property; may not be {@code null}
+     * @param type a {@link Class} representing the type to which to convert the values of the property; may not be {@code null}
      * and is expected to be one of the above types.
      * @param <T> the type-argument for {@code type}
      * @return the value of this property converted to the given type.
@@ -70,10 +113,21 @@ public interface Property extends javax.jcr.Property {
     public <T> T getAs(Class<T> type) throws ValueFormatException, RepositoryException;
 
     /**
-     * Converts the value of the property located at the given index to the specified type. This should only be used for multi-valued
-     * properties and <b>array types are not supported</b>
+     * Converts the value of the property located at the given index to the specified type. This can be used for single-valued
+     * properties if the <code>index</code> property is '0', or multi-valued properties. <b>Array types should not be supplied 
+     * as the first parameter.</b>
      * <p>
-     * For example: {@code getAs(Long.class, 1)}
+     * For example, this is a property usage of this method to obtain the value of a single-valued property or 
+     * the first value of a multi-valued property as a {@code long}:
+     * <pre>
+     *     Long value = prop.getAs(Long.class, 0)
+     * </pre>
+     * Likewise, the following usage will only work for multi-valued properties, since only a multi-valued property 
+     * can have 2 or more values:
+     * <pre>
+     *     Long value = prop.getAs(Long.class, 1)
+     * </pre>
+     * </p>
      *
      * @param type a {@link Class} representing the type to which to convert the value of the property; may not be {@code null}
      * and is expected to be one of the types from {@link Property#getAs(Class)}.
