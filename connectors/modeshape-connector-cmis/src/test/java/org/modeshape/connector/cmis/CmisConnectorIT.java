5b88f213b1669c6f8aa65ec6aef6af5c23c78110
vadym.karko <vadym.karko@engagepoint.com>
2014-02-06 16:53:28 +0200

    MODE-2017 Added to the CMIS connector support for moving nodes

--- connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/cmis/CmisConnectorIT.java
+++ connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/cmis/CmisConnectorIT.java
@@ -318,14 +318,16 @@ public class CmisConnectorIT extends MultiUseAbstractTest {
     @Test
     public void shouldBeAbleToMoveExternalNodes() throws Exception {
         assertNotNull(session.getNode("/cmis/My_Folder-0-0/My_Document-1-0"));
-        ((Workspace) session.getWorkspace()).move("/cmis/My_Folder-0-0/My_Document-1-0", "/cmis/My_Folder-0-0/My_Document-1-X");
-        Node file = session.getNode("/cmis/My_Folder-0-0/My_Document-1-X");
+        ((Workspace)session.getWorkspace()).move("/cmis/My_Folder-0-0/My_Document-1-0", "/cmis/My_Folder-0-1/My_Document-1-X");
+        Node file = session.getNode("/cmis/My_Folder-0-1/My_Document-1-X");
         assertNotNull(file);
         assertNotNull(session.getNode("/cmis/My_Folder-0-0"));
         ((Workspace) session.getWorkspace()).move("/cmis/My_Folder-0-0", "/cmis/My_Folder-0-X");
         Node folder = session.getNode("/cmis/My_Folder-0-X");
         assertNotNull(folder);
         assertEquals("nt:folder", folder.getPrimaryNodeType().getName());
-    }
-    
+        //undo the moves so that the original folder and document are unchaged (they are used by the other tests as well)
+        ((Workspace) session.getWorkspace()).move("/cmis/My_Folder-0-1/My_Document-1-X", "/cmis/My_Folder-0-X/My_Document-1-0");
+        ((Workspace) session.getWorkspace()).move("/cmis/My_Folder-0-X", "/cmis/My_Folder-0-0");
+    }    
 }
