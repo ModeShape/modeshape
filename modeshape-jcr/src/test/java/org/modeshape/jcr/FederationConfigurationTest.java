6ff9b28232c18ddaa62ccdc4f54dd50bbeb4d8f4
Horia Chiorean <hchiorea@redhat.com>
2014-03-21 14:30:56 +0200

    MODE-2175 Changed the vendor.url property so it doesn't get messed up in the productization process.

--- modeshape-jcr/src/test/java/org/modeshape/jcr/FederationConfigurationTest.java
+++ modeshape-jcr/src/test/java/org/modeshape/jcr/FederationConfigurationTest.java
@@ -57,18 +57,18 @@ public class FederationConfigurationTest extends SingleUseAbstractTest {
             session.save();
         }
 
-        // Add a node directly under 'federation/classes/org' (which is a federated node)...
-        Node org = federation.getNode("classes/org");
+        // Add a node directly under 'generated-sources' (which is a federated node)...
+        Node sources = federation.getNode("generated-sources");
         for (int i = 0; i != 3; ++i) {
-            Node newNode = org.addNode("GeneratedFolder_" + UUID.randomUUID().toString(), "nt:folder");
+            Node newNode = sources.addNode("GeneratedFolder_" + UUID.randomUUID().toString(), "nt:folder");
             assertThat(newNode, is(notNullValue()));
             session.save();
         }
 
-        // Add a node directly under 'federation/generated-sources' (which is a federated node)...
-        Node generated = federation.getNode("generated-sources");
+        // Add a node directly under 'generated-sources/annotations' (which is a federated node)...
+        Node annotations = federation.getNode("generated-sources/annotations");
         for (int i = 0; i != 3; ++i) {
-            Node newNode = generated.addNode("GeneratedFolder_" + UUID.randomUUID().toString(), "nt:folder");
+            Node newNode = annotations.addNode("GeneratedFolder_" + UUID.randomUUID().toString(), "nt:folder");
             assertThat(newNode, is(notNullValue()));
             session.save();
         }
