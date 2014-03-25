3851b5eb4a8f0d585bfab26411e9d51d1ecc1b32
Horia Chiorean <hchiorea@redhat.com>
2014-03-25 10:36:21 +0200

    MODE-2181 Changed the REST service tests to use Apache HttpClient. This allowed some previously disabled tests to be re-enabled.

--- web/modeshape-web-jcr-rest-war/src/test/java/org/modeshape/web/jcr/rest/ModeShapeRestServiceTest.java
+++ web/modeshape-web-jcr-rest-war/src/test/java/org/modeshape/web/jcr/rest/ModeShapeRestServiceTest.java
@@ -24,7 +24,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,6 +35,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.modeshape.common.FixFor;
 import org.modeshape.common.util.IoUtil;
+import org.modeshape.web.jcr.rest.form.FileUploadForm;
 import org.modeshape.web.jcr.rest.handler.RestBinaryHandler;
 
 /**
@@ -323,12 +323,10 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
         doPost((String)null, itemsUrl(TEST_NODE)).isCreated();
         doPost(fileStream("v2/post/binary.pdf"), binaryUrl(TEST_NODE, binaryPropertyName())).isCreated()
                                                                                             .isJSONObjectLikeFile(newBinaryProperty());
-        ByteArrayOutputStream actualBinaryContent = new ByteArrayOutputStream();
         Response response = doGet(binaryUrl(TEST_NODE, binaryPropertyName())).isOk();
-        IoUtil.write(response.content(), actualBinaryContent);
 
         byte[] expectedBinaryContent = IoUtil.readBytes(fileStream("v2/post/binary.pdf"));
-        assertArrayEquals(expectedBinaryContent, actualBinaryContent.toByteArray());
+        assertArrayEquals(expectedBinaryContent, response.contentAsBytes());
     }
 
     @Test
@@ -364,8 +362,8 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
     @Test
     public void shouldCreateBinaryValueViaMultiPartRequest() throws Exception {
         doPost((String)null, itemsUrl(TEST_NODE)).isCreated();
-        doPostMultiPart(fileStream("v2/post/binary.pdf"),
-                        "file",
+        doPostMultiPart("v2/post/binary.pdf",
+                        FileUploadForm.PARAM_NAME,
                         binaryUrl(TEST_NODE, binaryPropertyName()),
                         MediaType.APPLICATION_OCTET_STREAM).isCreated().isJSONObjectLikeFile(newBinaryProperty());
     }
@@ -395,13 +393,14 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
 
     @Test
     public void shouldImportCNDFileViaMultiPartRequest() throws Exception {
-        doPostMultiPart(fileStream(nodeTypesCND()), "file", nodeTypesUrl(), MediaType.TEXT_PLAIN).isOk()
-                                                                                                 .isJSONArrayLikeFile(cndImportResponse());
+        doPostMultiPart(nodeTypesCND(), FileUploadForm.PARAM_NAME, nodeTypesUrl(), MediaType.TEXT_PLAIN)
+                .isOk()
+                .isJSONArrayLikeFile(cndImportResponse());
     }
 
     @Test
     public void importingCNDViaWrongHTMLElementNameShouldFail() throws Exception {
-        doPostMultiPart(fileStream(nodeTypesCND()), "invalidHTML", nodeTypesUrl(), MediaType.TEXT_PLAIN).isBadRequest();
+        doPostMultiPart(nodeTypesCND(), "invalidHTML", nodeTypesUrl(), MediaType.TEXT_PLAIN).isBadRequest();
     }
 
     @Test
@@ -423,12 +422,10 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
     }
 
     @Test
-    @Ignore( "A limitation of HTTPUrlConnection prevents this test from running. Consider enabling it if/when switching to Http Client" )
     public void shouldDeleteMultipleNodes() throws Exception {
         doPost((String)null, itemsUrl(TEST_NODE)).isCreated();
         doPost("v2/post/multiple_nodes_request.json", itemsUrl()).isOk();
         doDelete("v2/delete/multiple_nodes_delete.json", itemsUrl()).isOk();
-        doGet(itemsUrl(TEST_NODE)).isJSONObjectLikeFile(nodeWithoutPrimaryTypeRequest());
     }
 
     private String binaryUrl( String... additionalPathSegments ) {
@@ -580,24 +577,29 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
          *   - child2
          *   - child3
          */
-        JSONObject children = doPost("v2/post/node_multiple_children_request.json", itemsUrl("node1")).isCreated().children();
-        String child2Id = children.getJSONObject("child2").getString(ID_KEY);
+        try {
+            JSONObject children = doPost("v2/post/node_multiple_children_request.json", itemsUrl("node1")).isCreated().children();
+            String child2Id = children.getJSONObject("child2").getString(ID_KEY);
 
-        /**
-         * node2
-         *   - childNode
-         */
-        JSONObject request = readJson("v2/post/node_hierarchy_request.json");
-        doPost(request, itemsUrl("node2")).isCreated();
+            /**
+             * node2
+             *   - childNode
+             */
+            JSONObject request = readJson("v2/post/node_hierarchy_request.json");
+            doPost(request, itemsUrl("node2")).isCreated();
 
-        JSONObject requestChildren = request.getJSONObject(CHILDREN_KEY);
-        request.remove("childNode");
-        requestChildren.put(child2Id, Collections.emptyMap());
+            JSONObject requestChildren = request.getJSONObject(CHILDREN_KEY);
+            request.remove("childNode");
+            requestChildren.put(child2Id, Collections.emptyMap());
 
-        //move node1/child2 to node2
-        doPut(request, itemsUrl("node2")).isOk();
-        assertTrue(doGet(itemsUrl("node2")).children().has("child2"));
-        assertFalse(doGet(itemsUrl("node1")).children().has("child2"));
+            //move node1/child2 to node2
+            doPut(request, itemsUrl("node2")).isOk();
+            assertTrue(doGet(itemsUrl("node2")).children().has("child2"));
+            assertFalse(doGet(itemsUrl("node1")).children().has("child2"));
+        } finally {
+            doDelete(itemsUrl("node1")).isDeleted();
+            doDelete(itemsUrl("node2")).isDeleted();
+        }
     }
 
     @Test
@@ -663,10 +665,9 @@ public class ModeShapeRestServiceTest extends JcrResourcesTest {
 
     @Test
     @FixFor( "MODE-2181" )
-    @Ignore( "A limitation of HTTPUrlConnection prevents this test from running. Consider enabling it if/when switching to Http Client" )
     public void shouldAllowDeletingSNS() throws Exception {
         doPost("v2/post/node_with_sns_request.json", itemsUrl(TEST_NODE)).isCreated();
-        doDelete("v2/delete/sns_nodes_delete.json", itemsUrl()).isDeleted();
+        doDelete("v2/delete/sns_nodes_delete.json", itemsUrl()).isOk();
         doGet(itemsUrl(TEST_NODE, "foo[1]")).isNotFound();
         doGet(itemsUrl(TEST_NODE, "foo[2]")).isNotFound();
     }
