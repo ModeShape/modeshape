21d27d1eb15ea8399196ef639d2fd48c7ee5ebc8
Horia Chiorean <hchiorea@redhat.com>
2014-08-05 15:08:30 +0300

    MODE-2271 Fixed the logic which processed child reorderings and update the parent document, since the previous code would cause the same child (children) to be added over and over again in the parent CHILDREN array, if the parent allowed SNS. The problem wasn't visible to client because of the "hack" for https://issues.jboss.org/browse/MODE-2120.

--- modeshape-jcr/src/main/java/org/modeshape/jcr/cache/document/DocumentTranslator.java
+++ modeshape-jcr/src/main/java/org/modeshape/jcr/cache/document/DocumentTranslator.java
@@ -790,7 +790,7 @@ public class DocumentTranslator implements DocumentConstants {
                                    Set<NodeKey> removals,
                                    Map<NodeKey, Name> newNames ) {
         List<?> children = document.getArray(CHILDREN);
-        EditableArray newChildren = Schematic.newArray();
+        LinkedHashSet<ChildReference> newChildren = new LinkedHashSet<ChildReference>();
         if (children != null) {
             // process existing children
             for (Object value : children) {
@@ -803,7 +803,7 @@ public class DocumentTranslator implements DocumentConstants {
                 Insertions insertions = insertionsByBeforeKey.remove(childKey);
                 if (insertions != null) {
                     for (ChildReference inserted : insertions.inserted()) {
-                        newChildren.add(fromChildReference(inserted));
+                        newChildren.add(inserted);
                     }
                 }
                 if (removals.remove(childKey)) {
@@ -813,10 +813,9 @@ public class DocumentTranslator implements DocumentConstants {
                     Name newName = newNames.get(childKey);
                     if (newName != null) {
                         // But has been renamed ...
-                        ChildReference newRef = ref.with(newName, 1);
-                        value = fromChildReference(newRef);
+                        ref = ref.with(newName, 1);
                     }
-                    newChildren.add(value);
+                    newChildren.add(ref);
                 }
             }
         }
@@ -848,12 +847,14 @@ public class DocumentTranslator implements DocumentConstants {
                     toBeInsertedInOrder.add(activeReference);
                 }
             }
-            for (ChildReference inserted : toBeInsertedInOrder) {
-                newChildren.add(fromChildReference(inserted));
-            }
+            newChildren.addAll(toBeInsertedInOrder);
         }
 
-        document.set(CHILDREN, newChildren);
+        EditableArray newChildrenArray = Schematic.newArray(newChildren.size());
+        for (ChildReference childReference : newChildren) {
+            newChildrenArray.add(fromChildReference(childReference));
+        }
+        document.set(CHILDREN, newChildrenArray);
         return newChildren.size();
     }
 
@@ -864,51 +865,21 @@ public class DocumentTranslator implements DocumentConstants {
         if (!hasChildren && !hasFederatedSegments) {
             return ImmutableChildReferences.EMPTY_CHILD_REFERENCES;
         }
+        ChildReferences internalChildRefs = hasChildren ?
+                                            ImmutableChildReferences.createLazy(this, document, CHILDREN) :
+                                            ImmutableChildReferences.EMPTY_CHILD_REFERENCES;
+        ChildReferences externalChildRefs = hasFederatedSegments ?
+                                            ImmutableChildReferences.createLazy(this, document, FEDERATED_SEGMENTS) :
+                                            ImmutableChildReferences.EMPTY_CHILD_REFERENCES;
 
-        boolean lazy = true;
-
-        if (!lazy) {
-            List<?> children = document.getArray(CHILDREN);
-            List<?> externalSegments = document.getArray(FEDERATED_SEGMENTS);
-
-            // Materialize the ChildReference objects in the 'children' document ...
-            List<ChildReference> internalChildRefsList = childReferencesListFromArray(children);
-
-            // Materialize the ChildReference objects in the 'federated segments' document ...
-            List<ChildReference> externalChildRefsList = childReferencesListFromArray(externalSegments);
-
-            // Now look at the 'childrenInfo' document for info about the next block of children ...
-            ChildReferencesInfo info = getChildReferencesInfo(document);
-            if (info != null) {
-                // The children are segmented ...
-                ChildReferences internalChildRefs = ImmutableChildReferences.create(internalChildRefsList);
-                ChildReferences externalChildRefs = ImmutableChildReferences.create(externalChildRefsList);
-
-                return ImmutableChildReferences.create(internalChildRefs, info, externalChildRefs, cache);
-            }
-            if (externalSegments != null) {
-                // There is no segmenting, so just add the federated references at the end
-                internalChildRefsList.addAll(externalChildRefsList);
-            }
-            return ImmutableChildReferences.create(internalChildRefsList);
+        // Now look at the 'childrenInfo' document for info about the next block of children ...
+        ChildReferencesInfo info = getChildReferencesInfo(document);
+        if (!hasChildren) {
+            return ImmutableChildReferences.create(externalChildRefs, info, cache);
+        } else if (!hasFederatedSegments) {
+            return ImmutableChildReferences.create(internalChildRefs, info, cache);
         } else {
-            ChildReferences internalChildRefs = hasChildren ?
-                                                ImmutableChildReferences.createLazy(this, document, CHILDREN) :
-                                                ImmutableChildReferences.EMPTY_CHILD_REFERENCES;
-            ChildReferences externalChildRefs = hasFederatedSegments ?
-                                                ImmutableChildReferences.createLazy(this, document, FEDERATED_SEGMENTS) :
-                                                ImmutableChildReferences.EMPTY_CHILD_REFERENCES;
-
-            // Now look at the 'childrenInfo' document for info about the next block of children ...
-            ChildReferencesInfo info = getChildReferencesInfo(document);
-            if (!hasChildren) {
-                return info != null ? ImmutableChildReferences.create(externalChildRefs, info, cache) : externalChildRefs;
-            } else if (!hasFederatedSegments) {
-                return info != null ? ImmutableChildReferences.create(internalChildRefs, info, cache) : internalChildRefs;
-            } else {
-                return info != null ? ImmutableChildReferences.create(internalChildRefs, info, externalChildRefs, cache) :
-                       ImmutableChildReferences.union(internalChildRefs, externalChildRefs);
-            }
+            return ImmutableChildReferences.create(internalChildRefs, info, externalChildRefs, cache);
         }
     }
 
