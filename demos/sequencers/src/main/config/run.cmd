c2b56983a1cf10c86e3f32781fb6ac7667004480
Horia Chiorean <hchiorea@redhat.com>
2016-02-23 11:33:37 +0200

    MODE-2570 Trims the modeshape binary distribution by removing the demos, test and test-source artifacts. It also updates the JavaDoc plugin number and configuration to run with JDK 8.

--- demos/sequencers/src/main/config/run.cmd
+++
@@ -1,1 +0,0 @@
-java -Dlogback.configurationFile=logback.xml -Djava.ext.dirs=./lib -cp ${project.artifactId}-${project.version}.jar org.modeshape.demo.sequencer.SequencerDemo
--- demos/sequencers/src/main/config/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Dlogback.configurationFile=logback.xml -Djava.ext.dirs=`pwd`/lib -cp ${project.artifactId}-${project.version}.jar org.modeshape.demo.sequencer.SequencerDemo
