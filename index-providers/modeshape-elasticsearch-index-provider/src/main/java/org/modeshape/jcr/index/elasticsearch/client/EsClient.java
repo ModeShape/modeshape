2240cdee1c98cefc03627a13bb1083b92d9c122d
kulikov <kulikov@kulikov>
2016-05-08 13:11:33 +0400

    MODE-2588: Replace HttpURLConnection with Apache Http client

--- index-providers/modeshape-elasticsearch-index-provider/src/main/java/org/modeshape/jcr/index/elasticsearch/client/EsClient.java
+++ index-providers/modeshape-elasticsearch-index-provider/src/main/java/org/modeshape/jcr/index/elasticsearch/client/EsClient.java
@@ -16,23 +16,32 @@
 package org.modeshape.jcr.index.elasticsearch.client;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.modeshape.schematic.document.Document;
 import org.modeshape.jcr.index.elasticsearch.query.MatchAllQuery;
 
 /**
  * HTTP-based interface for the Elasticsearch engine.
- * 
+ *
  * @author kulikov
  */
 public class EsClient {
+
     private final String host;
     private final int port;
-    
+
     /**
      * Creates new instance.
-     * 
+     *
      * @param host the address of the ES engine.
      * @param port the port number of ES engine.
      */
@@ -40,31 +49,28 @@ public class EsClient {
         this.host = host;
         this.port = port;
     }
-    
+
     /**
      * Tests for the index existence with specified name.
-     * 
+     *
      * @param name the name of the index to test.
      * @return true if index with given name exists and false otherwise.
      * @throws IOException communication exception.
      */
     public boolean indexExists(String name) throws IOException {
-        String url = String.format("http://%s:%d/%s", host, port, name);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpHead head = new HttpHead(String.format("http://%s:%d/%s", host, port, name));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("HEAD");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+            CloseableHttpResponse response = client.execute(head);
+            return response.getStatusLine().getStatusCode() == HttpStatus.SC_OK;
         } finally {
-            if (connection != null) connection.disconnect();
-        }        
+            head.releaseConnection();
+        }
     }
-    
+
     /**
      * Creates new index.
-     * 
+     *
      * @param name the name of the index to test.
      * @param type index type
      * @param mappings field mapping definition.
@@ -75,253 +81,230 @@ public class EsClient {
         if (indexExists(name)) {
             deleteIndex(name);
         }
-        
-        String url = String.format("http://%s:%d/%s", host, port, name);
-        HttpURLConnection connection = null;
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s", host, port, name));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setRequestMethod("PUT");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            if (mappings != null) {
-                mappings.write(connection.getOutputStream());
-            }            
-            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+            StringEntity requestEntity = new StringEntity(mappings.toString(), ContentType.APPLICATION_JSON);
+            method.setEntity(requestEntity);
+            CloseableHttpResponse resp = client.execute(method);
+            return resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK;
         } finally {
-            if (connection != null) connection.disconnect();
-        }        
+            method.releaseConnection();
+        }
     }
 
     /**
      * Deletes index.
-     * 
+     *
      * @param name the name of the index to be deleted.
      * @return true if index was deleted.
-     * @throws IOException 
+     * @throws IOException
      */
     public boolean deleteIndex(String name) throws IOException {
-        String url = String.format("http://%s:%d/%s", host, port, name);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpDelete delete = new HttpDelete(String.format("http://%s:%d/%s", host, port, name));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("DELETE");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+            CloseableHttpResponse resp = client.execute(delete);
+            return resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK;
         } finally {
-            if (connection != null) connection.disconnect();
+            delete.releaseConnection();
         }
     }
-    
+
     /**
      * Indexes document.
-     * 
+     *
      * @param name the name of the index.
      * @param type index type
      * @param id document id
      * @param doc document
      * @return true if document was indexed.
-     * @throws IOException 
+     * @throws IOException
      */
-    public boolean storeDocument(String name, String type, String id, 
+    public boolean storeDocument(String name, String type, String id,
             EsRequest doc) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setRequestMethod("PUT");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");            
-            doc.write(connection.getOutputStream());
-            return connection.getResponseCode() == HttpURLConnection.HTTP_CREATED;
+            StringEntity requestEntity = new StringEntity(doc.toString(), ContentType.APPLICATION_JSON);
+            method.setEntity(requestEntity);
+            CloseableHttpResponse resp = client.execute(method);
+            int statusCode = resp.getStatusLine().getStatusCode();
+            return statusCode == HttpStatus.SC_CREATED || statusCode == HttpStatus.SC_OK;
         } finally {
-            if (connection != null) connection.disconnect();
-        }        
+            method.releaseConnection();
+        }
     }
 
     /**
      * Searches indexed document.
-     * 
+     *
      * @param name index name.
      * @param type index type.
      * @param id document identifier.
      * @return document if it was found or null.
-     * @throws IOException 
+     * @throws IOException
      */
     public EsRequest getDocument(String name, String type, String id) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet method = new HttpGet(String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            switch (connection.getResponseCode()) {
-                case HttpURLConnection.HTTP_OK :
-                    EsResponse doc = EsResponse.read(connection.getInputStream());
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            switch (status) {
+                case HttpStatus.SC_OK :
+                    EsResponse doc = EsResponse.read(resp.getEntity().getContent());
                     return new EsRequest((Document) doc.get("_source"));
-                case HttpURLConnection.HTTP_NOT_FOUND :
-                case HttpURLConnection.HTTP_UNAVAILABLE :
+                case HttpStatus.SC_NOT_ACCEPTABLE:
+                case HttpStatus.SC_NOT_FOUND:
                     return null;
-                default :
-                    throw new IOException(connection.getResponseMessage());
+                default:
+                    throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
-    
+
     /**
      * Deletes document.
-     * 
+     *
      * @param name index name.
      * @param type index type.
      * @param id document id
      * @return true if it was deleted.
-     * @throws IOException 
+     * @throws IOException
      */
     public boolean deleteDocument(String name, String type, String id) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpDelete delete = new HttpDelete(String.format("http://%s:%d/%s/%s/%s", host, port, name, type, id));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("DELETE");
-            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+            return client.execute(delete).getStatusLine().getStatusCode() == HttpStatus.SC_OK;
         } finally {
-            if (connection != null) connection.disconnect();
+            delete.releaseConnection();
         }
     }
-    
+
     /**
      * Deletes all documents.
-     * 
+     *
      * @param name index name
      * @param type index type.
-     * @throws IOException 
+     * @throws IOException
      */
     public void deleteAll(String name, String type) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s", host, port, name, type);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/%s", host, port, name, type));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("DELETE");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
             EsRequest query = new EsRequest();
             query.put("query", new MatchAllQuery().build());
-            query.write(connection.getOutputStream());
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage());
+            StringEntity requestEntity = new StringEntity(query.toString(), ContentType.APPLICATION_JSON);
+            method.setEntity(requestEntity);
+            method.setHeader(" X-HTTP-Method-Override", "DELETE");
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
 
     /**
      * Flushes index data.
-     * 
+     *
      * @param name index name.
-     * @throws IOException 
+     * @throws IOException
      */
     public void flush(String name) throws IOException {
-        String url = String.format("http://%s:%d/%s/_flush", host, port, name);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/_flush", host, port, name));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage());
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
 
     /**
      * Refreshes index data.
-     * 
+     *
      * @param name index name.
-     * @throws IOException 
+     * @throws IOException
      */
     public void refresh(String name) throws IOException {
-        String url = String.format("http://%s:%d/%s/_refresh", host, port, name);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/_refresh", host, port, name));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage());
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
-    
+
     /**
      * Executes query.
-     * 
+     *
      * @param name index name.
      * @param type index type.
      * @param query query to be executed
      * @return search results in json format.
-     * @throws IOException 
+     * @throws IOException
      */
     public EsResponse search(String name, String type, EsRequest query) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s/_search", host, port, name, type);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/%s/_search", host, port, name, type));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            query.write(connection.getOutputStream());
-            
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage());
+            StringEntity requestEntity = new StringEntity(query.toString(), ContentType.APPLICATION_JSON);
+            method.setEntity(requestEntity);
+
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
-            
-            return EsResponse.read(connection.getInputStream());
+            return EsResponse.read(resp.getEntity().getContent());
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
 
     /**
      * Counts entries.
-     * 
+     *
      * @param name index name.
      * @param type index type.
      * @return number of indexed entries.
-     * @throws IOException 
+     * @throws IOException
      */
     public long count(String name, String type) throws IOException {
-        String url = String.format("http://%s:%d/%s/%s/_count", host, port, name, type);
-        HttpURLConnection connection = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost method = new HttpPost(String.format("http://%s:%d/%s/%s/_count", host, port, name, type));
         try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            
             EsRequest query = new EsRequest();
             query.put("query", new MatchAllQuery().build());
-            query.write(connection.getOutputStream());
-            
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage());
+            StringEntity requestEntity = new StringEntity(query.toString(), ContentType.APPLICATION_JSON);
+            method.setEntity(requestEntity);
+
+            CloseableHttpResponse resp = client.execute(method);
+            int status = resp.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                throw new IOException(resp.getStatusLine().getReasonPhrase());
             }
-            
-            return (Integer)EsResponse.read(connection.getInputStream()).get("count");
+            return (Integer) EsResponse.read(resp.getEntity().getContent()).get("count");
         } finally {
-            if (connection != null) connection.disconnect();
+            method.releaseConnection();
         }
     }
-}
 
+}
